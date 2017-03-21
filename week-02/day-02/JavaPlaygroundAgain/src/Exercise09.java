2061605c32c39caabd27d9bcad694f4ba4eb4717
zsiguli <markovichzsigmond@gmail.com>
2017-03-21 17:32:05 +0100

    Exercise09

---
+++ week-02/day-02/JavaPlaygroundAgain/src/Exercise09.java
@@ -0,0 +1,12 @@
+/**
+ * Created by markovich on 2017.03.21..
+ */
+public class Exercise09 {
+  public static void main(String[] args) {
+    double massInKg = 81.2;
+    double heightInM = 1.78;
+
+    System.out.println(massInKg / (heightInM * heightInM));
+
+  }
+}
