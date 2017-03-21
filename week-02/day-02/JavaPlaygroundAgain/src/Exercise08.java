0e793115a43e33a29d3ed7589b9d49bd45fc651f
zsiguli <markovichzsigmond@gmail.com>
2017-03-21 17:26:04 +0100

    Exercise(add): initial

---
+++ week-02/day-02/JavaPlaygroundAgain/src/Exercise08.java
@@ -0,0 +1,16 @@
+/**
+ * Created by markovich on 2017.03.21..
+ */
+public class Exercise08 {
+  public static void main(String[] args) {
+    String myName = "Zsiguli";
+    int myAge = 24;
+    double myHeight = 1.82;
+    boolean marriage = false;
+
+    System.out.println(myName);
+    System.out.println(myAge);
+    System.out.println(myHeight);
+    System.out.println(marriage);
+  }
+}
