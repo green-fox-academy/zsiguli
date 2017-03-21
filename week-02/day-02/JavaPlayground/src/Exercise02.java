4dd45a2ad06ee6c16770317152e3ab066f4bafe3
zsiguli <markovichzsigmond@gmail.com>
2017-03-21 14:30:54 +0100

    Exercise02(add):initial

---
+++ week-02/day-02/JavaPlayground/src/Exercise02.java
@@ -0,0 +1,11 @@
+/**
+ * Created by markovich on 2017.03.21..
+ */
+public class Exercise02 {
+  public static void main(String[] args) {
+    String hello = new String("Hello, ");
+    System.out.println(hello + "Imi!");
+    System.out.println(hello + "Zsombor!");
+    System.out.println(hello + "Zita!");
+  }
+}
