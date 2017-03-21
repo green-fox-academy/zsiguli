3f36f76497b74156845f0b2196d9fecd79ab9dda
zsiguli <markovichzsigmond@gmail.com>
2017-03-21 16:22:35 +0100

    Exercise05(feat): addition

---
+++ week-02/day-02/JavaPlaygroundAgain/src/Exercises05.java
@@ -0,0 +1,16 @@
+/**
+ * Created by markovich on 2017.03.21..
+ */
+public class Exercises05 {
+  public static void main(String[] args) {
+
+    int num1 = 22;
+    int num2 = 13;
+    int result;
+
+    result = num1 + num2;
+
+    System.out.println(result);
+
+  }
+}
