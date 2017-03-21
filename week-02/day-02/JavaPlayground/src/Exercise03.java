1841db9f622d3ea0be7bde7faadf43bcf5728253
zsiguli <markovichzsigmond@gmail.com>
2017-03-21 14:39:52 +0100

    Exercise04(add):initial

---
+++ week-02/day-02/JavaPlayground/src/Exercise03.java
@@ -0,0 +1,11 @@
+/**
+ * Created by markovich on 2017.03.21..
+ */
+public class Exercise03 {
+  public static void main(String[] args) {
+    System.out.println("Humpty Dumpty sat on a wall,");
+    System.out.println("Humpty Dumpty had a great fall.");
+    System.out.println("All the king's horses and all the king's men");
+    System.out.println("Couldn't put Humpty together again.");
+  }
+}
