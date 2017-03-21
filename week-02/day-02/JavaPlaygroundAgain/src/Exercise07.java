4bca70222deff9b7b0cef5b3dc75c27f61ec5965
zsiguli <markovichzsigmond@gmail.com>
2017-03-21 17:20:46 +0100

    Exercise07(add): initial

---
+++ week-02/day-02/JavaPlaygroundAgain/src/Exercise07.java
@@ -0,0 +1,9 @@
+/**
+ * Created by markovich on 2017.03.21..
+ */
+public class Exercise07 {
+  public static void main(String[] args) {
+    int favoriteNumber = 42;
+    System.out.println("My favorite number is " + favoriteNumber + "!");
+  }
+}
