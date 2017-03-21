29e451fa319960bd0124c5cc9f515501d8ed41cc
zsiguli <markovichzsigmond@gmail.com>
2017-03-21 16:15:37 +0100

    Exercise 04: initial

---
+++ week-02/day-02/JavaPlaygroundAgain/src/Exercise04.java
@@ -0,0 +1,10 @@
+/**
+ * Created by markovich on 2017.03.21..
+ */
+public class Exercise04 {
+  public static void main(String[] args) {
+    System.out.println("Zsiga");
+    System.out.println(24);
+    System.out.println(1.82);
+  }
+}
