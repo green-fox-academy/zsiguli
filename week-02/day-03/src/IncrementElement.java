dadff310a093248f1fcaedacb2be4823e0b3a32d
zsiguli <markovichzsigmond@gmail.com>
2017-03-22 14:52:34 +0100

    feat(increment): increment the third element and print is

---
+++ week-02/day-03/src/IncrementElement.java
@@ -0,0 +1,9 @@
+/**
+ * Created by markovich on 2017.03.22..
+ */
+public class IncrementElement {
+  public static void main(String[] args) {
+    int[] t = {1, 2, 3, 4, 5};
+    System.out.println(++t[2]);
+  }
+}
