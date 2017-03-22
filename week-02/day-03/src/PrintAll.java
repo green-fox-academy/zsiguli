0a25905db788afc99a1fd30a18993e327d7e4654
zsiguli <markovichzsigmond@gmail.com>
2017-03-22 15:05:05 +0100

    feat(print): pritnts the whole array

---
+++ week-02/day-03/src/PrintAll.java
@@ -0,0 +1,11 @@
+/**
+ * Created by markovich on 2017.03.22..
+ */
+public class PrintAll {
+  public static void main(String[] args) {
+    int[] af = {4, 5, 6, 7};
+    for (int num : af) {
+      System.out.print(num + " ");
+    }
+  }
+}
