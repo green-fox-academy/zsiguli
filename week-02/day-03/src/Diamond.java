eabfe38439ca5b68405d6b596e959e8023e0b5ad
zsiguli <markovichzsigmond@gmail.com>
2017-03-22 13:25:51 +0100

    initial commit

---
+++ week-02/day-03/src/Diamond.java
@@ -0,0 +1,11 @@
+/**
+ * Created by markovich on 2017.03.22..
+ */
+public class Diamond {
+  public static void main(String[] args) {
+    String one = "+";
+    String another = "-";
+
+    System.out.println(one);
+  }
+}
