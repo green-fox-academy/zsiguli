c88fbeb8b8551304b57702ac7f96c05c8e9e590e
zsiguli <markovichzsigmond@gmail.com>
2017-03-22 14:06:02 +0100

    feat(greet): can greet input

---
+++ week-02/day-03/src/Greet.java
@@ -0,0 +1,13 @@
+/**
+ * Created by markovich on 2017.03.22..
+ */
+public class Greet {
+  public static void main(String[] args) {
+    String al = "Greenfox";
+    greet(al);
+  }
+
+  public static void greet(String str) {
+    System.out.println("Greetings dear, " + str + "!");
+  }
+}
