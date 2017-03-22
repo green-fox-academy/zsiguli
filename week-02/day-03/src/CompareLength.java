872ccddb5550923015fe76c08f34266f605e19e3
zsiguli <markovichzsigmond@gmail.com>
2017-03-22 14:43:22 +0100

    feat(decision): print if p2 is bigger

---
+++ week-02/day-03/src/CompareLength.java
@@ -0,0 +1,12 @@
+/**
+ * Created by markovich on 2017.03.22..
+ */
+public class CompareLength {
+  public static void main(String[] args) {
+    int[] p1 = {1, 2, 3};
+    int[] p2 = {4, 5};
+    if (p1.length < p2.length) {
+      System.out.println("Print");
+    }
+  }
+}
