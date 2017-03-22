46307e4b984b746fa5f09e10f492c146766b32bd
zsiguli <markovichzsigmond@gmail.com>
2017-03-22 14:08:57 +0100

    feat(append): can append an 'a'

---
+++ week-02/day-03/src/AppendA.java
@@ -0,0 +1,13 @@
+/**
+ * Created by markovich on 2017.03.22..
+ */
+public class AppendA {
+  public static void main(String[] args) {
+    String am = "kuty";
+    appendA(am);
+  }
+
+  public static void appendA (String str) {
+    System.out.println(str + "a");
+  }
+}
