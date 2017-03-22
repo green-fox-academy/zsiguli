9a48af1b35ae267370a5f613c3167d3a45b2fecf
zsiguli <markovichzsigmond@gmail.com>
2017-03-22 16:45:23 +0100

    refactor(unecesary white space): remove unnecesary withspace

--- week-02/day-03/src/PrintArgument.java
+++ week-02/day-03/src/PrintArgument.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class PrintArgument {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    String [] strings = {scanner.nextLine(), scanner.nextLine(), scanner.nextLine()};
+    String[] strings = {scanner.nextLine(), scanner.nextLine(), scanner.nextLine()};
     printer(strings);
   }
   public static void printer (String... strArr) {
