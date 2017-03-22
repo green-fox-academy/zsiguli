46f93d5ee071a1252b5a74795f120501778debe9
zsiguli <markovichzsigmond@gmail.com>
2017-03-22 14:30:59 +0100

    feat(return): can return the given string up to 3

---
+++ week-02/day-03/src/PrintArgument.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Created by markovich on 2017.03.22..
+ */
+public class PrintArgument {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String [] strings = {scanner.nextLine(), scanner.nextLine(), scanner.nextLine()};
+    printer(strings);
+  }
+  public static void printer (String... strArr) {
+    for (int i = 0; i < strArr.length; i++) {
+      System.out.println((i + 1) + ". " + strArr[i]);
+    }
+  }
+}
