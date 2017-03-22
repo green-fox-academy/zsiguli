854e8ff195dea7130221c62bc20565ff54a1d9ae
zsiguli <markovichzsigmond@gmail.com>
2017-03-22 15:51:05 +0100

    feat(container): contain the shades of three colors

---
+++ week-02/day-03/src/Colors.java
@@ -0,0 +1,10 @@
+/**
+ * Created by markovich on 2017.03.22..
+ */
+public class Colors {
+  public static void main(String[] args) {
+    String[][]  colors = {{"lime", "forest green", "olive", "pale green", "spring green"},
+                          {"orange red", "red", "tomato"},
+                          {"orchid", "violet", "pink", "hot pink"}};
+  }
+}
