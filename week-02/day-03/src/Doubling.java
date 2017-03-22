a3ed4e3a00bac1359aa1387110aabcea789b279a
zsiguli <markovichzsigmond@gmail.com>
2017-03-22 14:01:14 +0100

    feat(double): can double input int

---
+++ week-02/day-03/src/Doubling.java
@@ -0,0 +1,13 @@
+/**
+ * Created by markovich on 2017.03.22..
+ */
+public class Doubling {
+  public static void main(String[] args) {
+    int ak = 123;
+    System.out.println(doubling(ak));
+  }
+
+  public static int doubling(int num) {
+    return num * 2;
+  }
+}
