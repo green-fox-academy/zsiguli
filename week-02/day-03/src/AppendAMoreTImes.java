591dc6fe2a10fcac409e7707c5357319912801eb
zsiguli <markovichzsigmond@gmail.com>
2017-03-22 16:01:26 +0100

    feat(append):append an for everithing

---
+++ week-02/day-03/src/AppendAMoreTImes.java
@@ -0,0 +1,11 @@
+/**
+ * Created by markovich on 2017.03.22..
+ */
+public class AppendAMoreTImes {
+  public static void main(String[] args) {
+    String[] nimals = {"kuty", "macs", "cic"};
+    for (int i = 0; i < nimals.length; i++) {
+      nimals[i] += "a";
+    }
+  }
+}
