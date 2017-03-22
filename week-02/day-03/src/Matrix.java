ec6a474e68c7e85906819f7daf4c1eab5d52f17f
zsiguli <markovichzsigmond@gmail.com>
2017-03-22 15:09:12 +0100

    feat(visula): dinamicali constructs the 4x4 matrix

--- week-02/day-03/src/Matrix.java
+++ week-02/day-03/src/Matrix.java
@@ -4,13 +4,16 @@
 public class Matrix {
   public static void main(String[] args) {
     int chosenOne = 0;
-
-    for (int i = 0; i < 4; i++) {
-      if (i == chosenOne) {
-        System.out.print(1 + " ");
-      } else {
-        System.out.print(0 + " ");
+    for (int j = 0; j < 4; j++) {
+      for (int i = 0; i < 4; i++) {
+        if (i == chosenOne) {
+          System.out.print(1 + " ");
+        } else {
+            System.out.print(0 + " ");
+        }
       }
+      chosenOne++;
+      System.out.println();
     }
   }
 }
