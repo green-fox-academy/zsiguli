04eae258367393080db9376331a13129b50c652a
zsiguli <markovichzsigmond@gmail.com>
2017-03-22 14:46:27 +0100

    feat(addition): add the second and third element from the given array and prints it

---
+++ week-02/day-03/src/SumElements.java
@@ -0,0 +1,9 @@
+/**
+ * Created by markovich on 2017.03.22..
+ */
+public class SumElements {
+  public static void main(String[] args) {
+    int[] r = {54, 23, 66, 12};
+    System.out.println(r[1] + r[2]);
+  }
+}
