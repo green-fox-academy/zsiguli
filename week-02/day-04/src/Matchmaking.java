3f1b9e5d339758b8af7c0bfd54a1893f0452aeb7
zsiguli <markovichzsigmond@gmail.com>
2017-03-23 16:05:22 +0100

    feat(boys list): merge boys with girls

--- week-02/day-04/src/Matchmaking.java
+++ week-02/day-04/src/Matchmaking.java
@@ -10,12 +10,19 @@ public class Matchmaking {
     ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe","Fred","Béla","Todd","Neef","Jeff"));
     ArrayList<String> order = new ArrayList<String>();
 
-    System.out.println(girlsOrder(girls, order));
+    System.out.println(boysOrder(boys, girlsOrder(girls, order)));
   }
   public static ArrayList<String> girlsOrder(ArrayList<String> girls, ArrayList<String > order) {
     for (int i = 0; i < girls.size(); i++) {
       order.add(i, girls.get(i));
     }
   return order;
+  }
+  public static ArrayList<String> boysOrder(ArrayList<String> boys, ArrayList<String > order) {
+    int counter = 0;
+    for (int i = 1; i < boys.size(); i += 2) {
+      order.add(i, boys.get(counter++));
+    }
+    return order;
   }
 }
