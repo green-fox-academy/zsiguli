285be3400a9f42a0470fe2b7779c92e9dd9e7ece
zsiguli <markovichzsigmond@gmail.com>
2017-03-23 11:55:17 +0100

    debug(replace): display the new value

---
+++ week-02/day-04/src/SimpleReplace.java
@@ -0,0 +1,10 @@
+/**
+ * Created by markovich on 2017.03.23..
+ */
+public class SimpleReplace {
+  public static void main(String[] args) {
+    String example = "In a dishwasher far far away";
+
+    System.out.println(example.replace("dishwasher", "galaxy"));
+  }
+}
