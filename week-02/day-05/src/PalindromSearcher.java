f6105376b6cb58ce3a8858238511ae6a31f0cefb
zsiguli <markovichzsigmond@gmail.com>
2017-03-24 10:20:31 +0100

    feat(string manipulation): inport existing code string reverser from day 4

--- week-02/day-05/src/PalindromSearcher.java
+++ week-02/day-05/src/PalindromSearcher.java
@@ -3,6 +3,22 @@
  */
 public class PalindromSearcher {
   public static void main(String[] args) {
-    
+    String input = "dog goat dad duck doodle never";
+    System.out.println(stringReverser(stringToBuilder(input)));
+  }
+
+  public static StringBuilder stringReverser(StringBuilder str) {
+    for (int i = 0; i < str.length() / 2; i++ ) {
+      char temp = str.charAt(i);
+      str.setCharAt(i, str.charAt(str.length()-1-i));
+      str.setCharAt(str.length() - 1 - i, temp );
+    }
+    return str;
+  }
+
+  public static StringBuilder stringToBuilder(String str) {
+    StringBuilder strB = new StringBuilder();
+    strB.append(str);
+    return strB;
   }
 }
