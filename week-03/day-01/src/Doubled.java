ea698ff5f2c3e02f88db8700c07b2ff9b287cfde
zsiguli <markovichzsigmond@gmail.com>
2017-03-27 19:28:19 +0200

    feat(input): read from file to a List

---
+++ week-03/day-01/src/Doubled.java
@@ -0,0 +1,21 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Doubled {
+  public static final String TXT_PATH = "assets/duplicated-chars.txt";
+
+  public static void main(String[] args) {
+    List<String> lines;
+    try {
+      Path filePath = Paths.get(TXT_PATH);
+      lines = Files.readAllLines(filePath);
+    } catch (Exception e) {
+      System.out.println("Uh-oh, an error happend: " + e.getClass());
+      lines = new ArrayList<>();
+    }
+    System.out.println(lines);
+  }
+}
