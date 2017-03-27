09d7dd83858551f6ae32f91e8ed9b6f297e945af
zsiguli <markovichzsigmond@gmail.com>
2017-03-27 13:40:34 +0200

    refact(inutData): parse useful data with a method

--- week-03/day-01/src/Lotto.java
+++ week-03/day-01/src/Lotto.java
@@ -2,18 +2,25 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by markovich on 2017.03.27..
  */
 public class Lotto {
   public static void main(String[] args) {
+    System.out.println(parseUsefulDataFromInput("otos.txt"));
+  }
+
+  public static List<String> parseUsefulDataFromInput(String inputFile) {
+    List<String> winnerNums = new ArrayList<>();
     try {
-      for (String line: Files.readAllLines(Paths.get("otos.txt"))) {
-      ArrayList<String> winnerNums = new ArrayList<>();
-      winnerNums.add(line.substring(line.lastIndexOf(("Ft")) + 3));
+      for (String line: Files.readAllLines(Paths.get(inputFile))) {
+        winnerNums.add(line.substring(line.lastIndexOf(("Ft")) + 3));
       }
     } catch (IOException e) {
-      System.out.println("Ooops something went wrong");
+      winnerNums.add("Ooops something went wrong");
     }
+    return winnerNums;
   }
 }
