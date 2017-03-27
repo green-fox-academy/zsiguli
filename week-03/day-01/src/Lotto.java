08f73dc6990f7f7c61e3d070ab06d040225675a5
zsiguli <markovichzsigmond@gmail.com>
2017-03-27 12:11:55 +0200

    feat(inputData): filter the useful part of the input data

--- week-03/day-01/src/Lotto.java
+++ week-03/day-01/src/Lotto.java
@@ -1,19 +1,17 @@
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by markovich on 2017.03.27..
  */
 public class Lotto {
   public static void main(String[] args) {
-    Path path = Paths.get("otos.txt");
     try {
-      List<String> list = Files.readAllLines(path);
-      System.out.println(list);
+      for (String line: Files.readAllLines(Paths.get("otos.txt"))) {
+      ArrayList<String> winnerNums = new ArrayList<>();
+      winnerNums.add(line.substring(line.lastIndexOf(("Ft")) + 3));
+      }
     } catch (IOException e) {
       System.out.println("Ooops something went wrong");
     }
