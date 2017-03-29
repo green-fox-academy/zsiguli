5bf73230ce41e223aca0cdeb9a42884f50cce824
zsiguli <markovichzsigmond@gmail.com>
2017-03-29 10:41:40 +0200

    feat(draw): draw different lines from give x, y to center

--- week-03/day-03/src/Drawing.java
+++ week-03/day-03/src/Drawing.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 
 import java.awt.*;
+
+import static java.lang.StrictMath.sin;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class Drawing {
@@ -17,16 +19,18 @@ public class Drawing {
     @Override
     protected void paintComponent(Graphics g) {
       super.paintComponent(g);
-      mainDraw(g);
+        mainDraw(g);
     }
   }
 
-  public static void mainDraw(Graphics g){
+  public static void mainDraw(Graphics g) {
+    extendedDrawer(g, 0, 0);
+  }
 
-    g.setColor(Color.GREEN);
-    g.drawLine(0,0,300,300);
-
-    g.setColor(Color.GREEN);
-    g.drawLine(0,300,300,0);
+  public static void extendedDrawer(Graphics g, int x, int y) {
+    for (int i = 0; i < 3; i++) {
+      g.setColor(Color.GREEN);
+      g.drawLine(x + 10 * i,y + i,150,150);
+    }
   }
 }
