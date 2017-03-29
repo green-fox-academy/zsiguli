1dac8680c1832897119f6fa7ca9bbc2f464e2aea
zsiguli <markovichzsigmond@gmail.com>
2017-03-29 15:17:14 +0200

    feat(draw): awesome catch ;) square and the fox

--- week-03/day-03/src/Drawing.java
+++ week-03/day-03/src/Drawing.java
@@ -24,12 +24,24 @@ public class Drawing {
   }
 
   public static void mainDraw(Graphics g) {
-    for (int i = 0; i <= 150; i += 10) {
-      g.setColor(Color.GREEN);
-      g.drawLine(0 + i , 150, 150, 150 - i);
-      g.drawLine(0 + i, 150, 150, 150 + i);
-      g.drawLine(300 - i, 150, 150, 150 - i);
-      g.drawLine(300 - i, 150, 150, 150 + i);
+   int[][] dots = {{10, 10}, {290,  10}, {290, 290}, {10, 290}};
+   int[][] anotherDots = {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
+                        {120, 100}, {85, 130}, {50, 100}};
+    try {
+      for (int i = 0; i < dots.length; ++i) {
+        g.drawLine(dots[i][0], dots[i][1], dots[i + 1][0], dots[i + 1][1]);
+      }
+    } catch (ArrayIndexOutOfBoundsException e) {
+        g.drawLine(dots[dots.length - 1][0], dots[dots.length - 1][1], dots[0][0], dots[0][1]);
+
+    }
+    try {
+      for (int i = 0; i < anotherDots.length; ++i) {
+        g.drawLine(anotherDots[i][0], anotherDots[i][1], anotherDots[i + 1][0], anotherDots[i + 1][1]);
+      }
+    } catch (ArrayIndexOutOfBoundsException e) {
+      g.drawLine(anotherDots[anotherDots.length - 1][0], anotherDots[anotherDots.length - 1][1], anotherDots[0][0], anotherDots[0][1]);
+
     }
   }
 }
