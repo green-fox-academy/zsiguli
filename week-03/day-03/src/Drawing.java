33bd8d8a8100f0ed5aa5731fb7a57db42443bb64
zsiguli <markovichzsigmond@gmail.com>
2017-03-29 15:30:44 +0200

    feat(draw): beautiful starry night

--- week-03/day-03/src/Drawing.java
+++ week-03/day-03/src/Drawing.java
@@ -19,29 +19,15 @@ public class Drawing {
     @Override
     protected void paintComponent(Graphics g) {
       super.paintComponent(g);
+      this.setBackground(Color.BLACK);
         mainDraw(g);
     }
   }
 
   public static void mainDraw(Graphics g) {
-   int[][] dots = {{10, 10}, {290,  10}, {290, 290}, {10, 290}};
-   int[][] anotherDots = {{50, 100}, {70, 70}, {80, 90}, {90, 90}, {100, 70},
-                        {120, 100}, {85, 130}, {50, 100}};
-    try {
-      for (int i = 0; i < dots.length; ++i) {
-        g.drawLine(dots[i][0], dots[i][1], dots[i + 1][0], dots[i + 1][1]);
-      }
-    } catch (ArrayIndexOutOfBoundsException e) {
-        g.drawLine(dots[dots.length - 1][0], dots[dots.length - 1][1], dots[0][0], dots[0][1]);
-
-    }
-    try {
-      for (int i = 0; i < anotherDots.length; ++i) {
-        g.drawLine(anotherDots[i][0], anotherDots[i][1], anotherDots[i + 1][0], anotherDots[i + 1][1]);
-      }
-    } catch (ArrayIndexOutOfBoundsException e) {
-      g.drawLine(anotherDots[anotherDots.length - 1][0], anotherDots[anotherDots.length - 1][1], anotherDots[0][0], anotherDots[0][1]);
-
-    }
+   for (int i = 0; i < 1000; ++i) {
+     g.setColor(new Color(255,255,(int)(Math.random() * 100) + 60));
+     g.drawRect((int)(Math.random() * 300), ((int)(Math.random() * 300)), 5, 5);
+   }
   }
 }
