4a959dd9299c7744811ccda24273ded251859e02
zsiguli <markovichzsigmond@gmail.com>
2017-03-30 14:23:05 +0200

    feat(hexa): init

---
+++ week-03/day-04/src/DrawHexagonFractal.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class DrawHexagonFractal {
+  final static int DIMENSION = 500;
+  final static int MAXFINALDIMENSION = 1;
+  final static int XCORD = 0;
+  final static int YCORD = 0;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(DIMENSION, DIMENSION));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics g) {
+      super.paintComponent(g);
+      mainDraw(g);
+      this.setBackground(Color.WHITE);
+    }
+  }
+
+  public static void mainDraw(Graphics g) {
+    extendedDrawer(g, XCORD, YCORD, DIMENSION);
+  }
+
+  public static void extendedDrawer(Graphics g, int x, int y, int dim) {
+
+  }
+}
+
