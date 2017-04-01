c2e8889dfb8bc5d09a07c22d10f7a6503ab61acc
zsiguli <markovichzsigmond@gmail.com>
2017-04-01 16:19:16 +0200

    feat(trees): print tree data with function

--- week-04/day-01/src/TreesWithVariables.java
+++ week-04/day-01/src/TreesWithVariables.java
@@ -8,13 +8,26 @@ public class TreesWithVariables {
   static List<String> Sexes = new ArrayList<>();
 
   public static void main(String[] args) {
-
+    newTree("A", "red", 15, "male");
+    newTree("B", "yellow", 1506, "male");
+    newTree("C", "green", 156, "female");
+    newTree("D", "blue", 4615, "male");
+    newTree("E", "purple", 185, "female");
+    printTreeData("A");
   }
 
-  public static void newTree(String type, String leafColor, Long age, String sex) {
+  public static void newTree(String type, String leafColor, long age, String sex) {
     Types.add(type);
     LeafColors.add(leafColor);
-    Ages.add(age);
+    Ages.add(new Long(age));
     Sexes.add(sex);
+  }
+
+  public static void printTreeData(String type) {
+    int index = Types.indexOf(type);
+    System.out.println("Type: " + Types.get(index) + "\n" +
+            "Leaf color: " + LeafColors.get(index) + "\n" +
+            "Age: " + Ages.get(index) + "\n" +
+            "Sex: " + Sexes.get(index));
   }
 }
