d3efaea7990a22eb0a5acef58de50d923c546c0f
zsiguli <markovichzsigmond@gmail.com>
2017-04-02 14:07:43 +0200

    feat(dead): pirates can die

--- week-04/day-01/src/Pirates.java
+++ week-04/day-01/src/Pirates.java
@@ -8,10 +8,12 @@ public class Pirates {
   String name;
   int rumCounter;
   int id;
+  boolean alive;
 
   public Pirates(String name) {
     this.id = birthCounter;
     rumCounter = 0;
+    alive = true;
 
     this.name = name;
 
@@ -22,6 +24,7 @@ public class Pirates {
   public Pirates() {
     this.id = birthCounter;
     rumCounter = 0;
+    alive = true;
 
     this.name = "Jack";
 
@@ -30,39 +33,61 @@ public class Pirates {
   }
 
   public static void drinkSomeRum(Pirates pirate, int rum) {
-    pirate.rumCounter = pirate.rumCounter + rum;
+    if (pirate.alive) {
+      pirate.rumCounter = pirate.rumCounter + rum;
+    } else {
+      System.out.println("he's dead");
+    }
   }
 
   public static void drinkSomeRum(Pirates pirate) {
-    pirate.rumCounter++;
+    if (pirate.alive) {
+      pirate.rumCounter++;
+    } else {
+      System.out.println("he's dead");
+    }
   }
 
   public static void drinkSomeRum() {
     for (Pirates pirate : alreadyBornPirates) {
-      pirate.rumCounter++;
+      if (pirate.alive) {
+        pirate.rumCounter++;
+      } else {
+        System.out.println("he's dead");
+      }
     }
   }
 
   public static void howsItGoingMate(Pirates pirate) {
-    if (pirate.rumCounter < 5) {
-      System.out.println("Pour me anudder!");
+    if (pirate.alive) {
+      if (pirate.rumCounter < 5) {
+        System.out.println("Pour me anudder!");
+      } else {
+        System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+        pirate.rumCounter = 0;
+      }
     } else {
-      System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
-      pirate.rumCounter = 0;
+      System.out.println("he's dead");
     }
+  }
+
+  public static void die(Pirates pirate) {
+    pirate.alive = false;
   }
 
   public static void main(String[] args) {
     Pirates pirate1 = new Pirates("Joe");
     Pirates pirate2 = new Pirates();
+    die(pirate2);
+    die(pirate1);
     drinkSomeRum();
     drinkSomeRum(pirate1);
     drinkSomeRum(pirate2, 6);
-    System.out.println(pirate1.name +  " " + pirate1.id + " " + pirate1.rumCounter);
-    System.out.println(pirate2.name +  " " + pirate2.id + " " + pirate2.rumCounter);
+    System.out.println(pirate1.name + " " + pirate1.id + " " + pirate1.rumCounter + " " + pirate1.alive);
+    System.out.println(pirate2.name + " " + pirate2.id + " " + pirate2.rumCounter + " " + pirate2.alive);
     howsItGoingMate(pirate1);
-    System.out.println(pirate1.name +  " " + pirate1.id + " " + pirate1.rumCounter);
+    System.out.println(pirate1.name + " " + pirate1.id + " " + pirate1.rumCounter + " " + pirate1.alive);
     howsItGoingMate(pirate2);
-    System.out.println(pirate2.name +  " " + pirate2.id + " " + pirate2.rumCounter);
+    System.out.println(pirate2.name + " " + pirate2.id + " " + pirate2.rumCounter + " " + pirate2.alive);
   }
 }
