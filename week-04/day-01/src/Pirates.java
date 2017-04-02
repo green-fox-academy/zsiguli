83298175c1d26e7b65468865cf772687a5cafdbb
zsiguli <markovichzsigmond@gmail.com>
2017-04-02 13:01:32 +0200

    feat(how-u-doing): pirates can answer how they are

--- week-04/day-01/src/Pirates.java
+++ week-04/day-01/src/Pirates.java
@@ -43,6 +43,15 @@ public class Pirates {
     }
   }
 
+  public static void howsItGoingMate(Pirates pirate) {
+    if (pirate.rumCounter < 5) {
+      System.out.println("Pour me anudder!");
+    } else {
+      System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+      pirate.rumCounter = 0;
+    }
+  }
+
   public static void main(String[] args) {
     Pirates pirate1 = new Pirates("Joe");
     Pirates pirate2 = new Pirates();
@@ -50,6 +59,10 @@ public class Pirates {
     drinkSomeRum(pirate1);
     drinkSomeRum(pirate2, 6);
     System.out.println(pirate1.name +  " " + pirate1.id + " " + pirate1.rumCounter);
+    System.out.println(pirate2.name +  " " + pirate2.id + " " + pirate2.rumCounter);
+    howsItGoingMate(pirate1);
+    System.out.println(pirate1.name +  " " + pirate1.id + " " + pirate1.rumCounter);
+    howsItGoingMate(pirate2);
     System.out.println(pirate2.name +  " " + pirate2.id + " " + pirate2.rumCounter);
   }
 }
