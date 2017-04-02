4d15ab5d9d6db2bd79ba50701ba25965d90f52a1
zsiguli <markovichzsigmond@gmail.com>
2017-04-02 11:13:52 +0200

    feat(trees2): make new Tree with constructor

--- week-04/day-01/src/TreesWithObjects.java
+++ week-04/day-01/src/TreesWithObjects.java
@@ -1,2 +1,19 @@
 public class TreesWithObjects {
+  public static void main(String[] args) {
+    Tree tree1 = new Tree("A", "red", 758, "male");
+  }
+}
+
+class Tree {
+  static String type;
+  static String leafColor;
+  static long age;
+  static String sex;
+
+  public Tree(String type, String leafColor, long age, String sex) {
+    this.type = type;
+    this.leafColor = leafColor;
+    this.age = age;
+    this.sex = sex;
+  }
 }
