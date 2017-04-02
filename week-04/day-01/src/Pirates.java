8e1db02796c5f9fe0a1c43086a4c890d20f1e605
zsiguli <markovichzsigmond@gmail.com>
2017-04-02 12:29:52 +0200

    feat(drink-rum): pirates can drink rum default value is 1, but with a plus int arg they can drink more at once

--- week-04/day-01/src/Pirates.java
+++ week-04/day-01/src/Pirates.java
@@ -23,9 +23,19 @@ public class Pirates {
     birthCounter++;
   }
 
+  public static void drinkSomeRum(Pirates pirate, int rum) {
+    pirate.rumCounter = pirate.rumCounter + rum;
+  }
+
+  public static void drinkSomeRum(Pirates pirate) {
+    pirate.rumCounter++;
+  }
+
   public static void main(String[] args) {
     Pirates pirate1 = new Pirates("Joe");
     Pirates pirate2 = new Pirates();
+    drinkSomeRum(pirate1);
+    drinkSomeRum(pirate2, 9);
     System.out.println(pirate1.name +  " " + pirate1.id + " " + pirate1.rumCounter);
     System.out.println(pirate2.name +  " " + pirate2.id + " " + pirate2.rumCounter);
   }
