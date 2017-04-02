d938b650674dfb13f570881d4cf12961026cd874
zsiguli <markovichzsigmond@gmail.com>
2017-04-02 12:18:09 +0200

    feat(born): create pirate with constructor constructor accept String or empty args

--- week-04/day-01/src/Pirates.java
+++ week-04/day-01/src/Pirates.java
@@ -1,2 +1,32 @@
 public class Pirates {
+  static int birthCounter;
+
+  String name;
+  int rumCounter;
+  int id;
+
+  public Pirates(String name) {
+    this.id = birthCounter;
+    rumCounter = 0;
+
+    this.name = name;
+
+    birthCounter++;
+  }
+
+  public Pirates() {
+    this.id = birthCounter;
+    rumCounter = 0;
+
+    this.name = "Jack";
+
+    birthCounter++;
+  }
+
+  public static void main(String[] args) {
+    Pirates pirate1 = new Pirates("Joe");
+    Pirates pirate2 = new Pirates();
+    System.out.println(pirate1.name +  " " + pirate1.id + " " + pirate1.rumCounter);
+    System.out.println(pirate2.name +  " " + pirate2.id + " " + pirate2.rumCounter);
+  }
 }
