4caa62b210d080c3c21dd3131f3199bf445d26a4
zsiguli <markovichzsigmond@gmail.com>
2017-04-03 11:29:59 +0200

    feat(fill-ship): fill ship with crew and a captain crew number is random 1-100

--- week-04/day-01/src/Ship.java
+++ week-04/day-01/src/Ship.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Ship {
 
-  List<Pirates> crew;
+  List<Pirates> crewList = new ArrayList<>();
   Pirates captain;
+
+  public void fillShip() {
+    captain = new Pirates();
+    int crewNumber = (int) (Math.random() * 100);
+    for (int i = 0; i < crewNumber; ++i) {
+      crewList.add(new Pirates());
+    }
+  }
 }
