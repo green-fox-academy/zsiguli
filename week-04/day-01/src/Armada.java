b9e1b66165c6884b92d3ce582ca1efc269992fba
zsiguli <markovichzsigmond@gmail.com>
2017-04-03 13:35:33 +0200

    feat(fill): fill armada with ships

--- week-04/day-01/src/Armada.java
+++ week-04/day-01/src/Armada.java
@@ -3,4 +3,11 @@ import java.util.List;
 
 public class Armada {
   List<Ship> ships = new ArrayList<>();
+
+  public void fillArmada() {
+    int shipNumber = (int) (Math.random() * 20);
+    for (int i = 0; i < shipNumber; ++i) {
+      ships.add(new Ship());
+    }
+  }
 }
