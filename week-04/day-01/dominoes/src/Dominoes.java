0c0e2777670f7190ac4184895df88c0731ed59aa
zsiguli <markovichzsigmond@gmail.com>
2017-04-03 15:51:11 +0200

    feat(order): return with first domino

--- week-04/day-01/dominoes/src/Dominoes.java
+++ week-04/day-01/dominoes/src/Dominoes.java
@@ -2,14 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Dominoes {
-  public static void main(String[] args) {
-    List<Domino> dominoes = initializeDominoes();
-    // You have the list of Dominoes
-    // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
-    // eg: [2, 4], [4, 3], [3, 5] ...
-
-    System.out.println(dominoes);
-  }
+  static List<Domino> orderedDominoes = new ArrayList<>();
 
   static List<Domino> initializeDominoes() {
     List<Domino> dominoes = new ArrayList<>();
@@ -21,4 +14,19 @@ public class Dominoes {
     dominoes.add(new Domino(7, 1));
     return dominoes;
   }
+
+  static List<Domino> orderDominoes(List<Domino> dominoes) {
+    if (orderedDominoes.size() == 0) {
+      orderedDominoes.add(dominoes.subList(0, 1).get(0));
+    }
+    return orderedDominoes;
+  }
+
+  public static void main(String[] args) {
+    List<Domino> dominoes = initializeDominoes();
+    List<Domino> orderedDominoes = orderDominoes(dominoes);
+    System.out.println(orderedDominoes);
+  }
+
+
 }
