c8439457016578f090a0fb65d6be86c6b0654c4b
zsiguli <markovichzsigmond@gmail.com>
2017-04-03 16:03:17 +0200

    feat(oreder): oreder dominoes

--- week-04/day-01/dominoes/src/Dominoes.java
+++ week-04/day-01/dominoes/src/Dominoes.java
@@ -2,30 +2,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Dominoes {
-  static List<Domino> orderedDominoes = new ArrayList<>();
 
   static List<Domino> initializeDominoes() {
     List<Domino> dominoes = new ArrayList<>();
     dominoes.add(new Domino(5, 2));
-    dominoes.add(new Domino(4, 6));
-    dominoes.add(new Domino(1, 5));
-    dominoes.add(new Domino(6, 7));
     dominoes.add(new Domino(2, 4));
+    dominoes.add(new Domino(4, 6));
+    dominoes.add(new Domino(6, 7));
     dominoes.add(new Domino(7, 1));
+    dominoes.add(new Domino(1, 5));
     return dominoes;
-  }
-
-  static List<Domino> orderDominoes(List<Domino> dominoes) {
-    if (orderedDominoes.size() == 0) {
-      orderedDominoes.add(dominoes.subList(0, 1).get(0));
-    }
-    return orderedDominoes;
   }
 
   public static void main(String[] args) {
     List<Domino> dominoes = initializeDominoes();
-    List<Domino> orderedDominoes = orderDominoes(dominoes);
-    System.out.println(orderedDominoes);
+    System.out.println(dominoes);
   }
 
 
