1d31fa497ccac9ee9dfc0dde5a5e0ee128ff309c
zsiguli <markovichzsigmond@gmail.com>
2017-04-04 13:54:52 +0200

    feat(constructor): ready

---
+++ week-04/day-02/garden/src/Plant.java
@@ -0,0 +1,13 @@
+public class Plant {
+  private String color;
+  private Double currentWaterAmount;
+  private Double waterAbsorbRate;
+
+  public Plant(String color, Double currentWaterAmount, Double waterAbsorbRate) {
+    this.color = color;
+    this.currentWaterAmount = currentWaterAmount;
+    this.waterAbsorbRate = waterAbsorbRate;
+  }
+
+  
+}
