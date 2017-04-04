5a33a0dd34e88b59bc7094c73049a1b802452343
zsiguli <markovichzsigmond@gmail.com>
2017-04-04 14:04:21 +0200

    feat(watering): can water plants with method

--- week-04/day-02/garden/src/Plant.java
+++ week-04/day-02/garden/src/Plant.java
@@ -3,11 +3,19 @@ public class Plant {
   private Double currentWaterAmount;
   private Double waterAbsorbRate;
 
-  public Plant(String color, Double currentWaterAmount, Double waterAbsorbRate) {
+  public Plant(String color, Double waterAbsorbRate) {
     this.color = color;
-    this.currentWaterAmount = currentWaterAmount;
     this.waterAbsorbRate = waterAbsorbRate;
+    currentWaterAmount = .0;
   }
 
-  
+  public Plant() {
+    color = "red";
+    waterAbsorbRate = 0.75;
+    currentWaterAmount = .0;
+  }
+
+  public void watering(int water) {
+    currentWaterAmount += water * waterAbsorbRate;
+  }
 }
