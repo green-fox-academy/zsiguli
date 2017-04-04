53f5d1d949a34f62424dc4cb7e62cf3e35f80ac0
zsiguli <markovichzsigmond@gmail.com>
2017-04-04 12:18:57 +0200

    feat(Student): student ready

--- week-04/day-02/green-fox-organisation/src/Student.java
+++ week-04/day-02/green-fox-organisation/src/Student.java
@@ -2,7 +2,31 @@ public class Student extends Person {
   private String previousOrganization;
   private int skippedDays;
 
+  public Student(String name, int age, String gender, String previousOrganization) {
+    super(name, age, gender);
+    this.previousOrganization = previousOrganization;
+    skippedDays = 0;
+  }
+
+  public Student() {
+    super();
+    previousOrganization = "The School of Life";
+    skippedDays = 0;
+  }
+
+  @Override
   public void getGoal() {
-    System.out.println("Be a junior software developer");
+    System.out.println("Be a junior software developer.");
+  }
+
+  @Override
+  public void introduce() {
+    System.out.println("Hi I'm " + getName() + ", a " + getAge() + " old " + getGender() +
+            " from " + previousOrganization + " who skipped " + skippedDays +
+            " days from the course already.");
+  }
+
+  public void skipDays() {
+    skippedDays++;
   }
 }
