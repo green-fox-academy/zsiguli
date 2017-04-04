1c40b56c8be555737c76682a3bd7d2fff08f9d04
zsiguli <markovichzsigmond@gmail.com>
2017-04-04 14:09:26 +0200

    remove(field): removed waterabsorbtionrate from plant

--- week-04/day-02/garden/src/Plant.java
+++ week-04/day-02/garden/src/Plant.java
@@ -1,21 +1,18 @@
 public class Plant {
   private String color;
   private Double currentWaterAmount;
-  private Double waterAbsorbRate;
 
-  public Plant(String color, Double waterAbsorbRate) {
+  public Plant(String color) {
     this.color = color;
-    this.waterAbsorbRate = waterAbsorbRate;
     currentWaterAmount = .0;
   }
 
   public Plant() {
     color = "red";
-    waterAbsorbRate = 0.75;
     currentWaterAmount = .0;
   }
 
   public void watering(int water) {
-    currentWaterAmount += water * waterAbsorbRate;
+    currentWaterAmount += water;
   }
 }
