3e1ede2d8f80c57404ac2bf478d292b77a0da3eb
zsiguli <markovichzsigmond@gmail.com>
2017-04-04 11:42:58 +0200

    feat(student): init fields + getGoal()

---
+++ week-04/day-02/green-fox-organisation/src/Student.java
@@ -0,0 +1,8 @@
+public class Student extends Person {
+  private String previousOrganization;
+  private int skippedDays;
+
+  public void getGoal() {
+    System.out.println("Be a junior software developer");
+  }
+}
