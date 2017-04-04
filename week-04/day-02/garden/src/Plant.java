d56accbde8df7e7009481c17f0f701152752cfec
zsiguli <markovichzsigmond@gmail.com>
2017-04-04 14:49:47 +0200

    feat(plant): redefined consturctors and fields

--- week-04/day-02/garden/src/Plant.java
+++ week-04/day-02/garden/src/Plant.java
@@ -1,18 +1,39 @@
 public class Plant {
   private String color;
   private Double currentWaterAmount;
+  private Double waterAbsorbRate;
+
+  public Plant(String color, Double waterAbsorbRate) {
+    this.color = color;
+    this.waterAbsorbRate = waterAbsorbRate;
+    currentWaterAmount = .0;
+  }
 
   public Plant(String color) {
     this.color = color;
+    waterAbsorbRate = .5;
     currentWaterAmount = .0;
   }
 
   public Plant() {
     color = "red";
+    waterAbsorbRate = .5;
     currentWaterAmount = .0;
   }
 
-  public void watering(int water) {
-    currentWaterAmount += water;
+  public Double getWaterAbsorbRate() {
+    return waterAbsorbRate;
+  }
+
+  public Double getCurrentWaterAmount() {
+    return currentWaterAmount;
+  }
+
+  public void setCurrentWaterAmount(Double currentWaterAmount) {
+    this.currentWaterAmount = currentWaterAmount;
+  }
+
+  public String getColor() {
+    return color;
   }
 }
