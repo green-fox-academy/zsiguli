1e65e818abdf4d5637545279afe01b43619a9259
zsiguli <markovichzsigmond@gmail.com>
2017-04-05 12:10:02 +0200

    feat(test-sum): test passed with list of numbers

--- week-04/day-03/test-exercises/tests/PlayGroundTest.java
+++ week-04/day-03/test-exercises/tests/PlayGroundTest.java
@@ -1,4 +1,7 @@
 import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import static org.junit.Assert.*;
 
 public class PlayGroundTest {
@@ -7,5 +10,13 @@ public class PlayGroundTest {
   public void apples() throws Exception {
     PlayGround playGround = new PlayGround();
     assertEquals("apple", playGround.getApple());
+  }
+
+  @Test
+  public void sum() throws Exception {
+    PlayGround a = new PlayGround();
+    List<Integer> numbers;
+    numbers = Arrays.asList(4,5,4,3,2);
+    assertEquals((long) 18, (long) a.sum(numbers));
   }
 }
