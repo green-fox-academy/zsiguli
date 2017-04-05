b4a7ed598bef44f7cec93e982299ff8a8365dd8b
zsiguli <markovichzsigmond@gmail.com>
2017-04-05 14:20:28 +0200

    feat(test-letter): work with normal string

--- week-04/day-03/test-exercises/tests/PlayGroundTest.java
+++ week-04/day-03/test-exercises/tests/PlayGroundTest.java
@@ -1,13 +1,31 @@
 import org.junit.Test;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
+import java.util.*;
+
 import static org.junit.Assert.*;
 
 public class PlayGroundTest {
 
   @Test
-  public void apples() throws Exception {
+  public void testLetterCounter() throws Exception {
+    Map<String, Integer> map = PlayGround.letterCounter("Test string!");
+    Map<String, Integer> controlMap = new HashMap<>();
+    controlMap.put("T", 1);
+    controlMap.put("e", 1);
+    controlMap.put("s", 2);
+    controlMap.put("t", 2);
+    controlMap.put(" ", 1);
+    controlMap.put("r", 1);
+    controlMap.put("i", 1);
+    controlMap.put("n", 1);
+    controlMap.put("g", 1);
+    controlMap.put("!", 1);
+
+    assertEquals(controlMap, map);
+  }
+
+  @Test
+  public void testApples() throws Exception {
     PlayGround playGround = new PlayGround();
     assertEquals("apple", playGround.getApple());
   }
