3cd8cfc9cffa14647fe1be68b4b596469c17acdc
zsiguli <markovichzsigmond@gmail.com>
2017-04-05 12:09:12 +0200

    feat(sum): sum given list of numbers

--- week-04/day-03/test-exercises/src/PlayGround.java
+++ week-04/day-03/test-exercises/src/PlayGround.java
@@ -1,5 +1,19 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PlayGround {
+
   public String getApple() {
     return "apple";
+  }
+
+  public Long sum(List<Integer> numbers) {
+    long result = 0;
+    for (Integer num : numbers) {
+      result += num;
+    }
+    return result;
   }
 }
