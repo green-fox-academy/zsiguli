3336f0bdd1297997bb31853aaa91ef90b2eda4ac
zsiguli <markovichzsigmond@gmail.com>
2017-04-05 14:39:14 +0200

    clone(extension): clone the original repo

---
+++ week-04/day-03/test-exercises/tests/ExtensionTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExtensionTest {
+  @Test
+  public void add() throws Exception {
+  }
+
+  @Test
+  public void maxOfThree() throws Exception {
+  }
+
+  @Test
+  public void median() throws Exception {
+  }
+
+  @Test
+  public void isVowel() throws Exception {
+  }
+
+  @Test
+  public void translate() throws Exception {
+  }
+
+}
