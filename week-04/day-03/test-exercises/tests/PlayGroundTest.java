fcdd8a20fefafc747b9c5cb148418e0191a2ba53
zsiguli <markovichzsigmond@gmail.com>
2017-04-05 12:18:36 +0200

    feat(test-sum): test with null and zero

--- week-04/day-03/test-exercises/tests/PlayGroundTest.java
+++ week-04/day-03/test-exercises/tests/PlayGroundTest.java
@@ -1,4 +1,6 @@
 import org.junit.Test;
+import org.omg.PortableServer.LIFESPAN_POLICY_ID;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,9 +16,24 @@ public class PlayGroundTest {
 
   @Test
   public void sum() throws Exception {
-    PlayGround a = new PlayGround();
+    PlayGround playGround = new PlayGround();
     List<Integer> numbers;
     numbers = Arrays.asList(4,5,4,3,2);
-    assertEquals((long) 18, (long) a.sum(numbers));
+    assertEquals((long) 18, (long) playGround.sum(numbers));
+  }
+
+  @Test
+  public void sumWithNull() throws Exception {
+    PlayGround playGround = new PlayGround();
+    List<Integer> numbers = new ArrayList<>();
+    assertEquals((long) 0, (long) playGround.sum(numbers));
+  }
+
+  @Test
+  public void sumWithZero() throws Exception {
+    PlayGround playGround = new PlayGround();
+    List<Integer> numbers = new ArrayList<>();
+    numbers.add(0);
+    assertEquals((long) 0, (long) playGround.sum(numbers));
   }
 }
