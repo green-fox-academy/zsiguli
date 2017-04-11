e73c4e617800d2e2b9d386b6b303a899bdef40cc
zsiguli <markovichzsigmond@gmail.com>
2017-04-11 15:13:40 +0200

    fix(map): remove random map generator

--- week-05/rpg-project/src/Board.java
+++ week-05/rpg-project/src/Board.java
@@ -7,7 +7,16 @@ public class Board extends JComponent implements KeyListener {
   static final int WIDTH_IN_SQUARES = 15;
   static final int HEIGHT_IN_SQUARES = 8;
   static final int DIMENSION = 72;
-  static final int[][] MAP = generateRandomMap();
+  static final int[][] MAP = {
+          {0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1},
+          {0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
+          {1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0},
+          {1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0},
+          {0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0},
+          {0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0},
+          {0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0},
+          {0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0}
+  };
   static String heroOrientation = "hero-down";
 
   int heroX;
@@ -19,49 +28,6 @@ public class Board extends JComponent implements KeyListener {
 
     setPreferredSize(new Dimension(WIDTH_IN_SQUARES * DIMENSION, HEIGHT_IN_SQUARES * DIMENSION));
     setVisible(true);
-  }
-
-  public static int[][] generateRandomMap() {
-    int[][] randomMap = new int[WIDTH_IN_SQUARES + 1][HEIGHT_IN_SQUARES + 1];
-    for (int i = 0; i <= WIDTH_IN_SQUARES; ++i) {
-      for (int j = 0; j <= HEIGHT_IN_SQUARES; ++j) {
-        if (i == 0 && j == 0) {
-          randomMap[i][j] = 0;
-        } else if (i == 1 && j == 0) {
-          randomMap[i][j] = 0;
-        } else if (i == 0) {
-          randomMap[i][j] = (int) (.5 + (Math.random()));
-        } else if (j == 0) {
-          randomMap[i][j] = (int) (.5 + (Math.random()));
-        } else if (isLastChance(randomMap, i, j)){
-          randomMap[i][j] = 0;
-        } else {
-          randomMap[i][j] = (int) (.8 + (Math.random()));
-        }
-      }
-    }
-    return randomMap;
-  }
-
-  public static boolean isLastChance(int[][] randomMap, int x, int y) {
-    if (randomMap[x - 1][y] == 0) {
-      try {
-        if (randomMap[x - 1][y - 1] == 0 && randomMap[x - 2][y] == 0) {
-          return false;
-        }
-      } catch (ArrayIndexOutOfBoundsException e) {
-        return true;
-      }
-    } else if (randomMap[x][y - 1] == 0){
-      try {
-        if (randomMap[x - 1][y - 1] == 0 && randomMap[x][y - 2] == 0) {
-          return false;
-        }
-      } catch (ArrayIndexOutOfBoundsException e) {
-        return true;
-      }
-    }
-    return true;
   }
 
   @Override
@@ -119,12 +85,12 @@ public class Board extends JComponent implements KeyListener {
 
 
   public void renderMap(Graphics graphics) {
-    for (int i = 0; i <= WIDTH_IN_SQUARES; ++i) {
-      for (int j = 0; j <= HEIGHT_IN_SQUARES; ++j) {
-        if (MAP[i][j] == 0) {
+    for (int i = 0; i < HEIGHT_IN_SQUARES; ++i) {
+      for (int j = 0; j < WIDTH_IN_SQUARES; ++j) {
+        if (MAP[j][i] == 0) {
           PositionedImage floor = new PositionedImage("img/floor.png", i * DIMENSION, j * DIMENSION);
           floor.draw(graphics);
-        } else if (MAP[i][j] == 1) {
+        } else if (MAP[j][i] == 1) {
           PositionedImage wall = new PositionedImage("img/wall.png", i * DIMENSION, j * DIMENSION);
           wall.draw(graphics);
         }
