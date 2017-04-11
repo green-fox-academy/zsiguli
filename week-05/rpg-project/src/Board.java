8a4fa5f7f4c0049645d23a16c47a35c89c269a16
zsiguli <markovichzsigmond@gmail.com>
2017-04-11 10:55:56 +0200

    refactor(dimensons): remove hard code, initialize finals to make the dimensions of the map

--- week-05/rpg-project/src/Board.java
+++ week-05/rpg-project/src/Board.java
@@ -4,6 +4,9 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Board extends JComponent implements KeyListener {
+  static final int WIDTH = 15;
+  static final int HEIGHT = 8;
+  static final int DIMENSION = 72;
 
   int testBoxX;
   int testBoxY;
@@ -12,38 +15,28 @@ public class Board extends JComponent implements KeyListener {
     testBoxX = 0;
     testBoxY = 0;
 
-    // set the size of your draw board
-    setPreferredSize(new Dimension(600, 600));
+    setPreferredSize(new Dimension(WIDTH * DIMENSION, HEIGHT * DIMENSION));
     setVisible(true);
   }
 
   @Override
   public void paint(Graphics graphics) {
     super.paint(graphics);
-    graphics.fillRect(testBoxX, testBoxY, 72, 72);
-    // here you have a 720x720 canvas
-    // you can create and draw an image using the class below e.g.
+    graphics.fillRect(testBoxX, testBoxY, DIMENSION, DIMENSION);
     PositionedImage image = new PositionedImage("img/floor.png", 0, 0);
     image.draw(graphics);
   }
 
   public static void boardMain() {
-    // Here is how you set up a new window and adding our board to it
     JFrame frame = new JFrame("RPG Game");
     Board board = new Board();
     frame.add(board);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setVisible(true);
     frame.pack();
-    // Here is how you can add a key event listener
-    // The board object will be notified when hitting any key
-    // with the system calling one of the below 3 methods
     frame.addKeyListener(board);
-    // Notice (at the top) that we can only do this
-    // because this Board class (the type of the board object) is also a KeyListener
   }
 
-  // To be a KeyListener the class needs to have these 3 methods in it
   @Override
   public void keyTyped(KeyEvent e) {
 
@@ -54,20 +47,23 @@ public class Board extends JComponent implements KeyListener {
 
   }
 
-  // But actually we can use just this one for our goals here
   @Override
   public void keyReleased(KeyEvent e) {
-    // When the up or down keys hit, we change the position of our box
     if (e.getKeyCode() == KeyEvent.VK_UP) {
-      testBoxY -= 72;
+      testBoxY -= DIMENSION;
     } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-      testBoxY += 72;
+      testBoxY += DIMENSION;
     } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-      testBoxX += 72;
+      testBoxX += DIMENSION;
     } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-      testBoxX -= 72;
+      testBoxX -= DIMENSION;
     }
-      // and redraw to have a new picture with the new coordinates
-      repaint();
+    repaint();
+  }
+
+  public void generateMap() {
+    for (int i = 0; i < WIDTH; i += DIMENSION) {
+
+    }
   }
 }
