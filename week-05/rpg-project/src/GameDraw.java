164ed523d6afbeb0841d30e6fc72620522a22370
zsiguli <markovichzsigmond@gmail.com>
2017-04-12 10:33:50 +0200

    refactor(map): map logic separated to a class

--- week-05/rpg-project/src/GameDraw.java
+++ week-05/rpg-project/src/GameDraw.java
@@ -7,16 +7,6 @@ public class GameDraw extends JComponent implements KeyListener {
   static final int WIDTH_IN_SQUARES = 15;
   static final int HEIGHT_IN_SQUARES = 8;
   static final int DIMENSION = 72;
-  static final int[][] MAP = {
-          {0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1},
-          {0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
-          {1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0},
-          {1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0},
-          {0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0},
-          {0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0},
-          {0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0},
-          {0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0}
-  };
 
   Character hero1 = new Hero();
   int[] temp1 = getRandomPosition();
@@ -61,7 +51,7 @@ public class GameDraw extends JComponent implements KeyListener {
   public void keyPressed(KeyEvent e) {
     if (e.getKeyCode() == KeyEvent.VK_UP) {
       if (hero1.actualPositionY > 0) {
-        if (MAP[hero1.actualPositionY - 1][hero1.actualPositionX] != 1) {
+        if (Map.map[hero1.actualPositionY - 1][hero1.actualPositionX] != 1) {
           --hero1.actualPositionY;
         }
       }
@@ -69,7 +59,7 @@ public class GameDraw extends JComponent implements KeyListener {
     } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
       try {
         if (hero1.actualPositionY < HEIGHT_IN_SQUARES) {
-          if (MAP[hero1.actualPositionY + 1][hero1.actualPositionX] != 1) {
+          if (Map.map[hero1.actualPositionY + 1][hero1.actualPositionX] != 1) {
             ++hero1.actualPositionY;
           }
         }
@@ -79,7 +69,7 @@ public class GameDraw extends JComponent implements KeyListener {
       hero1.setOrientation("hero-down");
     } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
       try {
-        if (MAP[hero1.actualPositionY][hero1.actualPositionX + 1] != 1) {
+        if (Map.map[hero1.actualPositionY][hero1.actualPositionX + 1] != 1) {
           if (hero1.actualPositionX < WIDTH_IN_SQUARES) {
             ++hero1.actualPositionX;
           }
@@ -89,7 +79,7 @@ public class GameDraw extends JComponent implements KeyListener {
       }
       hero1.setOrientation("hero-right");
     } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-      if (MAP[hero1.actualPositionY][hero1.actualPositionX - 1] != 1) {
+      if (Map.map[hero1.actualPositionY][hero1.actualPositionX - 1] != 1) {
         if (hero1.actualPositionX > 0) {
           --hero1.actualPositionX;
         }
@@ -108,10 +98,10 @@ public class GameDraw extends JComponent implements KeyListener {
   public void renderMap(Graphics graphics) {
     for (int i = 0; i < WIDTH_IN_SQUARES; ++i) {
       for (int j = 0; j < HEIGHT_IN_SQUARES; ++j) {
-        if (MAP[j][i] == 0) {
+        if (Map.map[j][i] == 0) {
           PositionedImage floor = new PositionedImage("img/floor.png", i * DIMENSION, j * DIMENSION);
           floor.draw(graphics);
-        } else if (MAP[j][i] == 1) {
+        } else if (Map.map[j][i] == 1) {
           PositionedImage wall = new PositionedImage("img/wall.png", i * DIMENSION, j * DIMENSION);
           wall.draw(graphics);
         }
@@ -134,7 +124,7 @@ public class GameDraw extends JComponent implements KeyListener {
     do {
       randomPosition[0] = (int) (Math.random() * HEIGHT_IN_SQUARES);
       randomPosition[1] = (int) (Math.random() * WIDTH_IN_SQUARES);
-    } while (MAP[randomPosition[0]][randomPosition[1]] == 1);
+    } while (Map.map[randomPosition[0]][randomPosition[1]] == 1);
     return randomPosition;
   }
 }
