d1351e50e97f2a2ddb624ae2f7bcf1b98ce0dd74
zsiguli <markovichzsigmond@gmail.com>
2017-04-14 23:35:53 +0200

    refactor(hero): movment

--- python/rpg_game/main.py
+++ python/rpg_game/main.py
@@ -11,31 +11,36 @@ class Game_object(object):
         self.costume_image = "img/" + str(costume_image) + ".png"
 
     def draw(self, canvas):
-        self.photo = PhotoImage(file=self.costume_image)
-        canvas.create_image(self.position_x, self.position_y, anchor = NW, image = self.photo)
+            self.photo = PhotoImage(file=self.costume_image)
+            canvas.create_image(self.position_x, self.position_y, anchor = NW, image = self.photo)
 
 root = Tk()
 root.wm_title("RPG Game")
 canvas = Canvas(root, width=dimension*width_in_squares, height=dimension*height_in_squares)
 
-game_object = Game_object(0, 0, "hero-down")
+game_objects = [Game_object(0, 0, "hero-down"), Game_object(50, 50, "hero-up")]
 
 def on_key_press(e):
+    hero = game_objects[0]
     if e.keycode == 9:
         sys.exit() 
     elif e.keycode == 111:
-        game_object.position_y -= dimension
+        hero.costum_image = "hero-down"
+        hero.position_y -= dimension
     elif e.keycode == 116:
-        game_object.position_y += dimension
+        game_objects[0].costum_image = "hero-up"
+        hero.position_y += dimension
     elif e.keycode == 113:
-        game_object.position_x -= dimension
+        hero.position_x -= dimension
     elif e.keycode == 114:
-        game_object.position_x += dimension
+        hero.position_x += dimension
 
-    game_object.draw(canvas)
+    for game_object in game_objects:
+        game_object.draw(canvas)
 
 canvas.bind("<KeyPress>", on_key_press)
 canvas.pack()
 canvas.focus_set()
-game_object.draw(canvas)
+for game_object in game_objects:
+    game_object.draw(canvas)
 root.mainloop()
