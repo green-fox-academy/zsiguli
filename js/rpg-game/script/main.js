a50dc8a1f814d6bc088254bf49dee9b9702ce800
zsiguli <markovichzsigmond@gmail.com>
2017-04-19 00:38:15 +0200

    feat(floor): generate floor on click

--- js/rpg-game/script/main.js
+++ js/rpg-game/script/main.js
@@ -1,7 +1,15 @@
 'use strict'
 
-document.querySelector('img').addEventListener("click", hide)
+const widthInSquares = 10
+const heightInSquars = 6
+const dimension = 72
 
-function hide() {
-  document.querySelector('img').classList.add("hidden")
+document.querySelector('img').addEventListener("click", fillFloor)
+
+function fillFloor() {
+  for (var i = 0; i < widthInSquares; i++) {
+    for (var j = 0; j < heightInSquars; j++) {
+      document.querySelector('main').innerHTML += "<img src='img/floor.png' alt='floor'>"
+    }
+  }
 }
