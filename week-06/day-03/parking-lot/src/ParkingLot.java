25b58774d2f4b25ce5b31ef923073ae214c86a12
zsiguli <markovichzsigmond@gmail.com>
2017-04-19 12:25:54 +0200

    feat(ParkingLot): create 256 random car

--- week-06/day-03/parking-lot/src/ParkingLot.java
+++ week-06/day-03/parking-lot/src/ParkingLot.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLot {
   public static void main(String[] args) {
-    
+    List<Car> randomlyCreatedCars = new ArrayList<>();
+    for (int i = 0; i < 256; ++i) {
+      randomlyCreatedCars.add(new Car());
+    }
   }
 }
