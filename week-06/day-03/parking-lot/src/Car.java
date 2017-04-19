58952f1366a0333994e2e2b6b02884ecc84a2d93
zsiguli <markovichzsigmond@gmail.com>
2017-04-19 12:06:52 +0200

    feat(Car): create getters to reach private fields

--- week-06/day-03/parking-lot/src/Car.java
+++ week-06/day-03/parking-lot/src/Car.java
@@ -1,10 +1,20 @@
 public class Car {
 
   private Enum type;
-  private Enum color;
 
+  private Enum color;
   public Car() {
     type = Type.MERCEDES;
     color = Color.BLACK;
   }
+
+  public Enum getType() {
+    return type;
+  }
+
+  public Enum getColor() {
+    return color;
+  }
+
+
 }
