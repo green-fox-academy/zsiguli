e369b9c5b89600abf4902cea3aa6c37bcb4beb12
zsiguli <markovichzsigmond@gmail.com>
2017-04-19 17:58:40 +0200

    feat(floor): render floor tiles without click

--- js/rpg-game/script/main.js
+++ js/rpg-game/script/main.js
@@ -4,12 +4,12 @@ const widthInSquares = 10
 const heightInSquars = 6
 const dimension = 72
 
-document.querySelector('img').addEventListener("click", fillFloor)
-
 function fillFloor() {
   for (var i = 0; i < widthInSquares; i++) {
     for (var j = 0; j < heightInSquars; j++) {
-      document.querySelector('main').innerHTML += "<img src='img/floor.png' alt='floor'>"
+      document.querySelector('main').innerHTML += "<img src='img/floor.png' alt='floor'> "
     }
   }
 }
+
+fillFloor()
