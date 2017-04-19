11730c4139dc08a8a839c3516a534abd2f8d5c56
zsiguli <markovichzsigmond@gmail.com>
2017-04-19 14:18:34 +0200

    feat(ParkingLot): print out color and type occurrences

--- week-06/day-03/parking-lot/src/ParkingLot.java
+++ week-06/day-03/parking-lot/src/ParkingLot.java
@@ -1,9 +1,41 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ParkingLot {
   public static void main(String[] args) {
-    List<Car> randomlyCreatedCars = createRandomCars(26);
+    List<Car> randomlyCreatedCars = createRandomCars(256);
+    Map<Enum, Integer> typeAndColorOccurrences = new HashMap<>();
+    for (Car car : randomlyCreatedCars) {
+      if (typeAndColorOccurrences.get(car.getType()) == null) {
+        typeAndColorOccurrences.put(car.getType(), 1);
+      } else {
+        int thing = typeAndColorOccurrences.get(car.getType());
+        typeAndColorOccurrences.put(car.getType(), ++thing);
+      }
+      if (typeAndColorOccurrences.get(car.getColor()) == null) {
+        typeAndColorOccurrences.put(car.getColor(), 1);
+      } else {
+        int thing = typeAndColorOccurrences.get(car.getColor());
+        typeAndColorOccurrences.put(car.getColor(), ++thing);
+      }
+    }
+    try {
+      for (int i = 0; i < typeAndColorOccurrences.size(); ++i) {
+        System.out.println(Type.values()[i] + ": " + typeAndColorOccurrences.get(Type.values()[i]));
+      }
+    } catch (ArrayIndexOutOfBoundsException ex) {
+
+    }
+
+    try {
+      for (int i = 0; i < typeAndColorOccurrences.size(); ++i) {
+        System.out.println(Color.values()[i] + ": " + typeAndColorOccurrences.get(Color.values()[i]));
+      }
+    } catch (ArrayIndexOutOfBoundsException ex) {
+
+    }
   }
 
   public static List<Car> createRandomCars(long numberOfCarsToCreate) {
