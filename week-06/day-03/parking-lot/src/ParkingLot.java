2b37a4f9308c18aea9a86f053a5435cfde4e39cf
zsiguli <markovichzsigmond@gmail.com>
2017-04-19 12:30:36 +0200

    refactor(ParkingLot): separate random car creatin to a method

--- week-06/day-03/parking-lot/src/ParkingLot.java
+++ week-06/day-03/parking-lot/src/ParkingLot.java
@@ -3,9 +3,14 @@ import java.util.List;
 
 public class ParkingLot {
   public static void main(String[] args) {
+    List<Car> randomlyCreatedCars = createRandomCars(26);
+  }
+
+  public static List<Car> createRandomCars(long numberOfCarsToCreate) {
     List<Car> randomlyCreatedCars = new ArrayList<>();
-    for (int i = 0; i < 256; ++i) {
+    for (int i = 0; i < numberOfCarsToCreate; ++i) {
       randomlyCreatedCars.add(new Car());
     }
+    return randomlyCreatedCars;
   }
 }
