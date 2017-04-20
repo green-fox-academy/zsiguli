eff2a6015bdf48be92a450a742b5668cc30d6a05
zsiguli <markovichzsigmond@gmail.com>
2017-04-20 18:12:25 +0200

    feat(map): generate map recursiveli

--- js/rpg-game/script/main.js
+++ js/rpg-game/script/main.js
@@ -2,23 +2,70 @@
 
 class Map {
 
- constructor() {
-    this.fields = Map.randomFields()
+  constructor() {
+    this.fields = Map.generateFields()
     this.floor = "<div class='floor'></div>"
     this.wall = "<div class='wall'></div>"
   }
 
-  static randomFields() {
-    var fields = [[0,1,1,0,1],[0,1,1,0,1],[0,1,1,0,1],[0,1,1,0,1],[0,1,1,0,1]]
-    return fields
+  static generateFields(fields) {
+    if (fields === undefined) {
+      fields = []
+    }
+      for (var i = 0; i < heightInSquares; ++i) {
+        if (fields[i] === undefined) {
+          fields[i] = []
+        }
+        for (var j = 0; j < widthInSquares; ++j) {
+          var random = 1.1 + Math.random()
+          if (fields[i][j] === 1) {
+          } else {
+            fields[i][j] = Math.floor(random)
+          }
+        }
+      }
+      if (Map.isOk(fields)) {
+        return fields
+      } else {
+        Map.generateFields(fields)
+      }
+      return fields
+  }
+
+  static isOk(fields) {
+    for (var i = 1; i < fields.length - 1; ++i) {
+      for (var j = 1; j < fields[i].length - 1; ++j) {
+        var counter = 0
+        if (fields[i][j] === 1) {
+          if (fields[i-1][j] === 1) {
+            ++counter
+          }
+          if (fields[i+1][j] === 1) {
+            ++counter
+          }
+          if (fields[i][j-1] === 1) {
+            ++counter
+          }
+          if (fields[i][j+1] === 1) {
+            ++counter
+          }
+          if (counter > 1) {
+            continue
+          } else {
+            return false
+          }
+        }
+      }
+    }
+    return true
   }
 
   fillFloor() {
     var temp
-    for (var i = 0; i < heightInSquares; i++) {
+    for (var i = 0; i < heightInSquares; ++i) {
       temp = "<section>"
-      for (var j = 0; j < widthInSquares; j++) {
-        this.fields[i][j] === 0 ? temp += this.floor : temp += this.wall
+      for (var j = 0; j < widthInSquares; ++j) {
+        this.fields[i][j] === 1 ? temp += this.floor : temp += this.wall
       }
       temp += "</section>"
       document.querySelector('main').innerHTML += temp
@@ -27,8 +74,8 @@ class Map {
 
 }
 
-const widthInSquares = 5
-const heightInSquares = 5
+const widthInSquares = 18
+const heightInSquares = 8
 const dimension = 72
 
 var map = new Map()
