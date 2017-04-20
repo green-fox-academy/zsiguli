d0a72fcbe8825c3564af6d4dcbc46b5f7c5d11c2
zsiguli <markovichzsigmond@gmail.com>
2017-04-20 20:42:59 +0200

    refactor(Life): remove unnecesary code parts

--- js/game-of-life/main.js
+++ js/game-of-life/main.js
@@ -1,92 +1,11 @@
 'use strict'
 
-class Map {
+class Life {
 
   constructor() {
-    this.fields = Map.generateFields()
+    this.fields = [[1,2,1,2,1,2,1,2],[1,2,1,2,1,2,1,2],[1,2,1,2,1,2,1,2],[1,2,1,2,1,2,1,2]]
     this.floor = "<div class='floor'></div>"
     this.wall = "<div class='wall'></div>"
-  }
-
-  static generateFields(fields) {
-    if (fields === undefined) {
-      fields = []
-    }
-    for (var i = 0; i < heightInSquares; ++i) {
-      if (fields[i] === undefined) {
-        fields[i] = []
-      }
-      for (var j = 0; j < widthInSquares; ++j) {
-        var random = 1.5 + Math.random()
-        if (fields[i][j] === 1) {
-        } else {
-          fields[i][j] = Math.floor(random)
-        }
-      }
-    }
-    Map.filter(fields)
-    if (Map.isOk(fields)) {
-      return fields
-    } else {
-      Map.generateFields(fields)
-    }
-    return fields
-  }
-
-  static isOk(fields) {
-    for (var i = 1; i < fields.length - 1; ++i) {
-      for (var j = 1; j < fields[i].length - 1; ++j) {
-        var counter = 0
-        if (fields[i][j] === 1) {
-          if (fields[i-1][j] === 1) {
-            ++counter
-          }
-          if (fields[i+1][j] === 1) {
-            ++counter
-          }
-          if (fields[i][j-1] === 1) {
-            ++counter
-          }
-          if (fields[i][j+1] === 1) {
-            ++counter
-          }
-          if (counter > 1) {
-            continue
-          } else {
-            return false
-          }
-        }
-      }
-    }
-    return true
-  }
-
-  static filter(fields) {
-    for (var i = 1; i < fields.length - 1; ++i) {
-      for (var j = 1; j < fields[i].length - 1; ++j) {
-        var counter = 0
-        if (fields[i][j] === 1) {
-          if (fields[i-1][j] === 1) {
-            ++counter
-          }
-          if (fields[i+1][j] === 1) {
-            ++counter
-          }
-          if (fields[i][j-1] === 1) {
-            ++counter
-          }
-          if (fields[i][j+1] === 1) {
-            ++counter
-          }
-          if (counter <= 1) {
-            fields[i][j] = 2
-          } else {
-            continue
-          }
-        }
-      }
-    }
-    return fields
   }
 
   fillFloor() {
@@ -105,5 +24,5 @@ class Map {
 const widthInSquares = 18
 const heightInSquares = 9
 
-var map = new Map()
-map.fillFloor()
+var life = new Life()
+life.fillFloor()
