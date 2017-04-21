744939242d6df24931e9b2a1505025b14bf0a08f
zsiguli <markovichzsigmond@gmail.com>
2017-04-21 12:02:10 +0200

    init(Task): add Task class

---
+++ week-06/day-05/to-do-app/src/main/java/Task.java
@@ -0,0 +1,9 @@
+public class Task {
+  private boolean done;
+  private String description;
+
+  public Task(String description) {
+    this.done = false;
+    this.description = description;
+  }
+}
