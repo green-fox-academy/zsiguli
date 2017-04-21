363b4655311a9eeea2b98431f679ec73ff9008c3
zsiguli <markovichzsigmond@gmail.com>
2017-04-21 11:30:40 +0200

    init(AddCommand)

---
+++ week-06/day-05/to-do-app/src/main/java/AddCommand.java
@@ -0,0 +1,5 @@
+public class AddCommand extends Command {
+  public AddCommand() {
+    super("a", "Adds a new task");
+  }
+}
--- week-06/day-05/to-do-app/src/main/java/ToDoApp.java
+++ week-06/day-05/to-do-app/src/main/java/ToDoApp.java
@@ -1,4 +1,3 @@
-import java.io.File;
 import java.util.List;
 
 public class ToDoApp {
