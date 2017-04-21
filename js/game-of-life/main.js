3d07b05f7ef49344b5476745634b0e8cbf50f0eb
zsiguli <markovichzsigmond@gmail.com>
2017-04-21 10:11:13 +0200

    pause(life): change back to java toDoApp

--- js/game-of-life/main.js
+++ js/game-of-life/main.js
@@ -1,9 +1,10 @@
 'use strict'
-
+var cc = 1
 class Life {
 
   constructor() {
-    this.aliveTabel = [[2,1,2,2,2],[2,1,2,2,2],[2,1,2,2,2],[2,2,2,2,1],[2,2,2,1,1],]
+    // this.aliveTabel = [[1,1,2,2,2],[1,1,2,2,2],[2,2,2,2,2],[2,2,2,2,2],[2,2,2,2,2],]
+    this.aliveTabel = [[2,2,2,2,2],[2,1,2,2,2],[2,1,2,2,2],[2,1,2,2,2],[2,2,2,2,2],]
     this.floor = "<div class='floor'></div>"
     this.wall = "<div class='wall'></div>"
   }
@@ -13,22 +14,36 @@ class Life {
     for (var y = 0; y < this.aliveTabel.length; y++) {
       for (var x = 0; x < this.aliveTabel[y].length; x++) {
         var aliveCounter = 0
+        var deathCounter = 0
         for (var i = -1; i <= 1; ++i) {
           for (var j = -1; j <= 1; ++j) {
+            if (i === 0 && j === 0) {
+              continue
+            }
             try {
               if (temp[y+i][x+j] === 1) {
                 ++aliveCounter
               }
             } catch (e) {
+                ++deathCounter
+            }
+            try {
+              if (temp[y+i][x+j] === 2) {
+                ++deathCounter
+              }
+            } catch (e) {
+              
             }
           }
         }
-        if (temp[y][x] === 1) {
-          --aliveCounter
-        }
+        console.log('dead: ' + deathCounter + ', alive: ' + aliveCounter);
+        // if (temp[y][x] === 1) {
+          // --aliveCounter
+        // }
+        console.log(cc++ + ': ' + aliveCounter)
         if (temp[y][x] === 1 && (aliveCounter < 2 || aliveCounter > 3)) {
         temp[y][x] = 2
-        } else if (aliveCounter === 3) {
+        } else if (temp[y][x] === 2 && aliveCounter === 3) {
           temp[y][x] = 1
         }
       }
