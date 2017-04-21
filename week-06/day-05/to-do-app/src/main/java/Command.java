de5e43e3332208d7849a6946f8dfb0a058cead39
zsiguli <markovichzsigmond@gmail.com>
2017-04-21 11:23:52 +0200

    feat(Command): define methods

--- week-06/day-05/to-do-app/src/main/java/Command.java
+++ week-06/day-05/to-do-app/src/main/java/Command.java
@@ -6,4 +6,14 @@ public abstract class Command {
     this.flag = flag;
     this.usage = usage;
   }
+
+  public void execute() {};
+
+  public String getFlag() {
+    return flag;
+  }
+
+  public String getUsage() {
+    return " -" + flag + " " + usage;
+  }
 }
