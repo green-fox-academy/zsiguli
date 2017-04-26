0721e2d2c110bf4d97046983d6004e271f10df32
zsiguli <markovichzsigmond@gmail.com>
2017-04-26 14:07:58 +0200

    fix(FileIO): correct error message

--- week-06/day-05/to-do-app/src/main/java/FileIO.java
+++ week-06/day-05/to-do-app/src/main/java/FileIO.java
@@ -8,22 +8,22 @@ import java.util.List;
 public class FileIO {
   private static final Path STORE_PATH = Paths.get("data/store.csv");
 
-  public static List<String> getListFromFile() {
+  public static List<String> getTasksFromFile() {
     List<String> listFromFile;
     try {
       listFromFile = Files.readAllLines(STORE_PATH);
     } catch (IOException e) {
       listFromFile = new ArrayList<>();
-      System.out.println("Something wrong happened, during the file read. Check assets/store.csv");
+      System.out.println("Something wrong happened, during the file read. Check data/store.csv");
     }
     return listFromFile;
   }
 
-  public static void writeListToFile(List<String> listToWrite) {
+  public static void writeTasksToFile(List<String> listToWrite) {
     try {
       Files.write(STORE_PATH, listToWrite);
     } catch (IOException e) {
-      System.out.println("Something wrong happened, during the file write. Check assets/store.csv");
+      System.out.println("Something wrong happened, during the file write. Check data/store.csv");
     }
   }
 }
