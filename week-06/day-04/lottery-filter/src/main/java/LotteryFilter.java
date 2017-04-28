2b5e3fd5f714438dd6535e53eee6c8135527c74a
zsiguli <markovichzsigmond@gmail.com>
2017-04-28 18:10:49 +0200

    feat(LotteryFilter): handle all the possible args

--- week-06/day-04/lottery-filter/src/main/java/LotteryFilter.java
+++ week-06/day-04/lottery-filter/src/main/java/LotteryFilter.java
@@ -4,6 +4,19 @@ import joptsimple.OptionSet;
 public class LotteryFilter {
   public static void main(String[] args) {
     OptionParser parser = new OptionParser("y:f:o:");
+
     OptionSet options = parser.parse(args);
+
+    if (options.has("y")) {
+      System.out.println(options.valueOf("y"));
+    }
+
+    if (options.has("f")) {
+      System.out.println(options.valueOf("f"));
+    }
+
+    if (options.has("o")) {
+      System.out.println(options.valueOf("o"));
+    }
   }
 }
