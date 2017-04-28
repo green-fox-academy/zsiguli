2866c584ad3851c92241dc20ef6e81ccf509995b
zsiguli <markovichzsigmond@gmail.com>
2017-04-28 18:45:32 +0200

    feat(LotteryFilter): show filtered data

--- week-06/day-04/lottery-filter/src/main/java/LotteryFilter.java
+++ week-06/day-04/lottery-filter/src/main/java/LotteryFilter.java
@@ -1,5 +1,10 @@
+import au.com.bytecode.opencsv.CSVReader;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class LotteryFilter {
   public static void main(String[] args) {
@@ -9,6 +14,21 @@ public class LotteryFilter {
 
     if (options.has("y")) {
       System.out.println(options.valueOf("y"));
+      CSVReader reader = null;
+      try {
+        reader = new CSVReader(new FileReader("data/otos.csv"), ';');
+      } catch (FileNotFoundException e) {
+        e.printStackTrace();
+      }
+      String [] nextLine;
+      try {
+        while ((nextLine = reader.readNext()) != null) {
+          if (nextLine[0].equals(options.valueOf("y")))
+          System.out.println(nextLine[0] + nextLine[1]);
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
 
     if (options.has("f")) {
