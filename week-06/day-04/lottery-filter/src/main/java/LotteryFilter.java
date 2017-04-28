1dac3ca22dbb606e5028328007c5626cb68e8262
zsiguli <markovichzsigmond@gmail.com>
2017-04-28 17:45:32 +0200

    feat(LotteryFilter): import jOpt to handle args

--- week-06/day-04/lottery-filter/src/main/java/LotteryFilter.java
+++ week-06/day-04/lottery-filter/src/main/java/LotteryFilter.java
@@ -1,5 +1,9 @@
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
 public class LotteryFilter {
   public static void main(String[] args) {
-    
+    OptionParser parser = new OptionParser("y:f:o:");
+    OptionSet options = parser.parse(args);
   }
 }
