6d3e71229ab49838b449a5ed111f620ca1fa5360
zsiguli <markovichzsigmond@gmail.com>
2017-04-29 15:01:12 +0200

    feat(YodaSpeak): read input from cli

--- week-06/day-04/YodaSpeak/src/main/java/YodaSpeak.java
+++ week-06/day-04/YodaSpeak/src/main/java/YodaSpeak.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class YodaSpeak {
   public static void main(String[] args) {
-    
+    Scanner scanner = new Scanner(System.in);
+    System.out.println(scanner.next());
   }
 }
