d5574d6d8991e73185c2eefc3be08149f7b9c774
zsiguli <markovichzsigmond@gmail.com>
2017-04-29 15:26:41 +0200

    feat(YodaSpeak): create proper request link

--- week-06/day-04/YodaSpeak/src/main/java/YodaSpeak.java
+++ week-06/day-04/YodaSpeak/src/main/java/YodaSpeak.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class YodaSpeak {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.println(scanner.nextLine());
+    String sentence = scanner.nextLine().replace(" ", "+");
+    String URLLink = "https://yoda.p.mashape.com/yoda?sentence=" + sentence;
+    System.out.println(URLLink);
   }
 }
