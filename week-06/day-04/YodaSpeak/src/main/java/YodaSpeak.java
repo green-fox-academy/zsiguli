77b2c6fa1d1fd7201baee815b6e65871645dcd33
zsiguli <markovichzsigmond@gmail.com>
2017-04-29 15:04:07 +0200

    fix(YodaSpeak): scan whole line not just the first thing

--- week-06/day-04/YodaSpeak/src/main/java/YodaSpeak.java
+++ week-06/day-04/YodaSpeak/src/main/java/YodaSpeak.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class YodaSpeak {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.println(scanner.next());
+    System.out.println(scanner.nextLine());
   }
 }
