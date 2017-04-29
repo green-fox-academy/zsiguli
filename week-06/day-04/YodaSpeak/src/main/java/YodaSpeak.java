4d37bc8bab0333366c03e56228ebed400c0757ee
zsiguli <markovichzsigmond@gmail.com>
2017-04-29 15:29:49 +0200

    refactor(YodaSpeak): rename variable

--- week-06/day-04/YodaSpeak/src/main/java/YodaSpeak.java
+++ week-06/day-04/YodaSpeak/src/main/java/YodaSpeak.java
@@ -1,10 +1,12 @@
+import java.net.URL;
 import java.util.Scanner;
 
 public class YodaSpeak {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
-    String sentence = scanner.nextLine().replace(" ", "+");
-    String URLLink = "https://yoda.p.mashape.com/yoda?sentence=" + sentence;
+    String inputSentence = scanner.nextLine().replace(" ", "+");
+    String URLLink = "https://yoda.p.mashape.com/yoda?sentence=" + inputSentence;
+    URL url = new URL(URLLink);
     System.out.println(URLLink);
   }
 }
