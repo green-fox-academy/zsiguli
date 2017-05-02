8f5b9a448d862eb03c4107d0b0ce8162fb8538ec
zsiguli <markovichzsigmond@gmail.com>
2017-05-02 11:58:56 +0200

    feat(greeting): greets the user in diff languages

--- week-07/day-02/first-springboot-app/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-02/first-springboot-app/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -7,8 +7,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWebController {
+  final static String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+          "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+          "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+          "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
   @RequestMapping("/web/greeting")
   static public String greeting(Model model, @RequestParam("name") String name) {
+    int random = (int) (Math.random() * hellos.length);
+    model.addAttribute("hello", hellos[random]);
     model.addAttribute("name", name);
     model.addAttribute("loadCounter", HelloRESTController.numberOfRequests.incrementAndGet());
     return "greeting";
