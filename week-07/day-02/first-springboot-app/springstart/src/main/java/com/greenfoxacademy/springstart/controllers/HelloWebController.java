0f2860e220cbdaeaa66af235c86ecb20da572abb
zsiguli <markovichzsigmond@gmail.com>
2017-05-02 15:25:48 +0200

    feat(controller+template): show hellos in diff colors and size

--- week-07/day-02/first-springboot-app/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-02/first-springboot-app/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -11,13 +11,21 @@ public class HelloWebController {
           "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
           "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
           "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+  final static String[] colors = {"red", "blue", "grey", "black", "yellow", "green"};
+
+  final static int[] fontSizes = {10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60};
+
   @RequestMapping("/web/greeting")
   static public String greeting(Model model, @RequestParam("name") String name) {
-    int random = (int) (Math.random() * hellos.length);
-    model.addAttribute("hello", hellos[random]);
+    int randomHello = (int) (Math.random() * hellos.length);
+    int randomColor = (int) (Math.random() * colors.length);
+    int randomFontSize = (int) (Math.random() * fontSizes.length);
+    model.addAttribute("hello", hellos[randomHello]);
     model.addAttribute("name", name);
     model.addAttribute("loadCounter", HelloRESTController.numberOfRequests.incrementAndGet());
-    model.addAttribute("color", "red");
+    model.addAttribute("color", colors[randomColor]);
+    model.addAttribute("fontSize", fontSizes[randomFontSize]);
     return "greeting";
   }
 }
