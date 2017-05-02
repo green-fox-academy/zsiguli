4d989db8020984b1748ae5323205bcd510392056
zsiguli <markovichzsigmond@gmail.com>
2017-05-02 11:15:08 +0200

    feat(HelloRESTController): count requests

--- week-07/day-02/first-springboot-app/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-02/first-springboot-app/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
+  static AtomicLong numberOfRequests = new AtomicLong();
   @RequestMapping(value = "/greeting")
   static public Greeting greeting(@RequestParam("name") String name) {
-    Greeting greeting = new Greeting(1, "Hello " + name);
+    Greeting greeting = new Greeting(numberOfRequests.incrementAndGet(), "Hello " + name);
     return greeting;
   }
 }
