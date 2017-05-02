c88b10cf771b5283709c301f2bdcd316c69aae26
zsiguli <markovichzsigmond@gmail.com>
2017-05-02 11:45:15 +0200

    feat(greeting): greets the user and indicates the pageloading since server start

--- week-07/day-02/first-springboot-app/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-02/first-springboot-app/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,12 +3,14 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWebController {
   @RequestMapping("/web/greeting")
-  static public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  static public String greeting(Model model, @RequestParam("name") String name) {
+    model.addAttribute("name", name);
+    model.addAttribute("loadCounter", HelloRESTController.numberOfRequests.incrementAndGet());
     return "greeting";
   }
 }
