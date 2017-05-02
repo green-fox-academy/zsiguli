8b83fa78317d0e523976f2d456de5a6e2c1cbe10
zsiguli <markovichzsigmond@gmail.com>
2017-05-02 10:57:19 +0200

    feat(HelloRESTController): greet the given person

--- week-07/day-02/first-springboot-app/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-02/first-springboot-app/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,13 +1,14 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
   @RequestMapping(value = "/greeting")
-  static public Greeting greeting() {
-    Greeting greeting = new Greeting(1, "Hello World");
+  static public Greeting greeting(@RequestParam("name") String name) {
+    Greeting greeting = new Greeting(1, "Hello " + name);
     return greeting;
   }
 }
