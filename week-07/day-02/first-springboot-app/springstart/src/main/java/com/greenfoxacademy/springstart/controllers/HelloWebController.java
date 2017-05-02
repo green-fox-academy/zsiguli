a70cc565019679d27cd253f190868cdae95a1337
zsiguli <markovichzsigmond@gmail.com>
2017-05-02 11:18:38 +0200

    feat(HelloWebController): add greeting method

--- week-07/day-02/first-springboot-app/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-02/first-springboot-app/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -1,4 +1,12 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class HelloWebController {
+  @RequestMapping("/web/greeting")
+  public String greeting(Model model) {
+    model.addAttribute("name", " World");
+    return "greeting";
+  }
 }
