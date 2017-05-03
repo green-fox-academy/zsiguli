b41247fd4294223bd1ca1386e27248615b3f090f
zsiguli <markovichzsigmond@gmail.com>
2017-05-03 10:32:50 +0200

    feat(Exercise5): list every account from list

--- week-07/day-03/exercise1/src/main/java/com/greenfox/controller/WebAppController.java
+++ week-07/day-03/exercise1/src/main/java/com/greenfox/controller/WebAppController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class WebAppController {
 
@@ -34,6 +37,17 @@ public class WebAppController {
     String content = "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>";
     model.addAttribute("content", content);
     return "twoDifferentTextType";
+  }
+
+  @RequestMapping(value = "/exercise5")
+  public static String manyBankAccounts(Model model) {
+    List<BankAccount> accounts = new ArrayList<>();
+    accounts.add(new BankAccount("Simba", 2000, "lion"));
+    accounts.add(new BankAccount("Pumba", 10000, "warthog"));
+    accounts.add(new BankAccount("Rafiki", 100, "monkey"));
+    accounts.add(new BankAccount("Zazu", 2000, "bird"));
+    model.addAttribute("accounts", accounts);
+    return "manyBankAccounts";
   }
 
 }
