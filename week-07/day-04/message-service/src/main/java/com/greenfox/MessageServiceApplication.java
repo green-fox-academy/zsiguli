b51edde4ddc32cd9572ae55485724050c0106ad2
zsiguli <markovichzsigmond@gmail.com>
2017-05-04 13:26:35 +0200

    feat(MessageServiceApp): implement CommandLineRunner

--- week-07/day-04/message-service/src/main/java/com/greenfox/MessageServiceApplication.java
+++ week-07/day-04/message-service/src/main/java/com/greenfox/MessageServiceApplication.java
@@ -1,12 +1,23 @@
 package com.greenfox;
 
+import com.greenfox.configuration.MessageProceeder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MessageServiceApplication {
+public class MessageServiceApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MessageServiceApplication.class, args);
-	}
+  @Autowired
+  MessageProceeder messageProceeder;
+
+  public static void main(String[] args) {
+    SpringApplication.run(MessageServiceApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    messageProceeder.processMessage("Hi Barba, How are you?", "office@greenfox.com");
+  }
 }
