8d9df9b03345a87e9f320576faceb3296629fbc2
zsiguli <markovichzsigmond@gmail.com>
2017-05-04 13:57:34 +0200

    feat(EmailService): work as described

--- week-07/day-04/message-service/src/main/java/com/greenfox/service/EmailService.java
+++ week-07/day-04/message-service/src/main/java/com/greenfox/service/EmailService.java
@@ -4,6 +4,6 @@ public class EmailService implements MessageService {
 
   @Override
   public void sendingMessage(String text, String address) {
-
+    System.out.println("Email sent from: " + address + " with message=" + text);
   }
 }
