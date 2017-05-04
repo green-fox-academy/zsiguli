594469c167eacbebee7e3c33ebe0f535f06c7434
zsiguli <markovichzsigmond@gmail.com>
2017-05-04 11:16:59 +0200

    feat(HelloWorldApp): implements cmlrunner works properly

--- week-07/day-04/hello-world/src/main/java/com/greenfox/HelloWorldApplication.java
+++ week-07/day-04/hello-world/src/main/java/com/greenfox/HelloWorldApplication.java
@@ -2,11 +2,12 @@ package com.greenfox;
 
 import com.greenfox.model.HelloWorld;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloWorldApplication {
+public class HelloWorldApplication implements CommandLineRunner{
 
   @Autowired
   HelloWorld helloWorld;
@@ -15,6 +16,7 @@ public class HelloWorldApplication {
     SpringApplication.run(HelloWorldApplication.class, args);
   }
 
+  @Override
   public void run(String... args) throws Exception {
     helloWorld.setMessage("Hello World!");
     helloWorld.getMessage();
