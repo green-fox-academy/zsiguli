731ee5480eeba554b0194f09b931469058b179d4
zsiguli <markovichzsigmond@gmail.com>
2017-05-04 11:55:21 +0200

    fix(RedColor): add @Component annotation

--- week-07/day-04/coloring-beans/src/main/java/com/greenfox/RedColor.java
+++ week-07/day-04/coloring-beans/src/main/java/com/greenfox/RedColor.java
@@ -1,6 +1,9 @@
 package com.greenfox;
 
-public class RedColor implements MyColor {
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedColor implements MyColor{
   @Override
   public void printColor() {
     System.out.println("I'am the red one!");
