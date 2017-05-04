eb495a5921f3b6d04709499d2dbcddaff8833e94
zsiguli <markovichzsigmond@gmail.com>
2017-05-04 11:55:58 +0200

    feat(ColoringApplication): sout a list of colors

--- week-07/day-04/coloring-beans/src/main/java/com/greenfox/ColoringBeansApplication.java
+++ week-07/day-04/coloring-beans/src/main/java/com/greenfox/ColoringBeansApplication.java
@@ -5,11 +5,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class ColoringBeansApplication implements CommandLineRunner{
 
   @Autowired
   RedColor redColor;
+  @Autowired
+  BlueColor blueColor;
+  @Autowired
+  GreenColor greenColor;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ColoringBeansApplication.class, args);
@@ -17,6 +24,13 @@ public class ColoringBeansApplication implements CommandLineRunner{
 
   @Override
   public void run(String... args) throws Exception {
+    List<MyColor> colors = new ArrayList<>();
+    colors.add(redColor);
+    colors.add(blueColor);
+    colors.add(greenColor);
 
+    for (MyColor color : colors) {
+      color.printColor();
+    }
   }
 }
