8699db81b8beae608ad2962d7a15972eecb78dff
zsiguli <markovichzsigmond@gmail.com>
2017-05-04 17:57:27 +0200

    feat(page-render): connect template with object

--- week-07/day-05/programmer-fox-club/src/main/java/com/greenfox/controller/MainController.java
+++ week-07/day-05/programmer-fox-club/src/main/java/com/greenfox/controller/MainController.java
@@ -1,12 +1,20 @@
 package com.greenfox.controller;
 
+import com.greenfox.model.Fox;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-  @RequestMapping(value = "/")
-  public static String homePage() {
+
+  @Autowired
+  Fox fox;
+
+  @GetMapping(value = "/")
+  public String homePage(Model model) {
+    model.addAttribute("fox", fox);
     return "index";
   }
 }
--- week-07/day-05/programmer-fox-club/src/main/java/com/greenfox/model/Fox.java
+++ week-07/day-05/programmer-fox-club/src/main/java/com/greenfox/model/Fox.java
@@ -1,13 +1,22 @@
 package com.greenfox.model;
 
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
+@Getter
 public class Fox {
   private String name;
   private List<String> knownTricks;
   private String food;
   private String drink;
+
+  public Fox() {
+    knownTricks = new ArrayList<>();
+    knownTricks.add("this");
+    knownTricks.add("that");
+  }
 }
