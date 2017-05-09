9610790d70d0f9aec7d639671db67720d66934e8
zsiguli <markovichzsigmond@gmail.com>
2017-05-09 09:38:51 +0200

    tips(rpg-game): new trick from a frend

--- week-05/rpg-project/java/src/GameObject.java
+++ week-05/rpg-project/java/src/GameObject.java
@@ -1,3 +1,12 @@
+import java.awt.*;
+
 public class GameObject {
-  
+  //Transform transform;
+
 }
+//public class Transform
+//{
+//  Point position;
+//  Point direction;
+//  void Translate(){}
+//}
