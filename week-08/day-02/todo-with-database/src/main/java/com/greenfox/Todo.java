ef20b1908c8be057e2bd8b942816cca5705a5cbc
zsiguli <markovichzsigmond@gmail.com>
2017-05-09 09:56:33 +0200

    chore(todo): add todo class

---
+++ week-08/day-02/todo-with-database/src/main/java/com/greenfox/Todo.java
@@ -0,0 +1,8 @@
+package com.greenfox;
+
+public class Todo {
+  int id;
+  String title;
+  boolean isUrgent;
+  boolean isDone;
+}
---
+++ week-08/day-02/todo-with-database/src/main/java/com/greenfox/TodoWithDatabaseApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoWithDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoWithDatabaseApplication.class, args);
+	}
+}
