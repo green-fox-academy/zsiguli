73c6f99848234c7747d46b158838e229b340d372
zsiguli <markovichzsigmond@gmail.com>
2017-05-09 15:44:46 +0200

    feat(exercise1): working as described

--- week-08/day-02/todo-with-database/src/main/java/com/greenfox/controllers/TodoController.java
+++ week-08/day-02/todo-with-database/src/main/java/com/greenfox/controllers/TodoController.java
@@ -1,15 +1,30 @@
 package com.greenfox.controllers;
 
+import com.greenfox.model.Todo;
+import com.greenfox.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TodoController {
-  @GetMapping(value = "/list")
-  @ResponseBody
-  public String list(Model model) {
-    return "This is my first todo";
+
+  @Autowired
+  TodoRepository todoRepository;
+
+  @RequestMapping("/")
+  public String listTodos(Model model) {
+    model.addAttribute("todos", todoRepository.findAll());
+    return "todoslist";
+  }
+
+  @RequestMapping("/addTodo")
+  public String addTodo(Model model,
+                        @RequestParam(value = "description") String description) {
+    todoRepository.save(new Todo(description));
+    model.addAttribute("todos", todoRepository.findAll());
+    return "todoslist";
   }
 }
--- week-08/day-02/todo-with-database/src/main/java/com/greenfox/model/Todo.java
+++ week-08/day-02/todo-with-database/src/main/java/com/greenfox/model/Todo.java
@@ -1,8 +1,31 @@
 package com.greenfox.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Todos")
+@Getter
+@Setter
 public class Todo {
-  int id;
-  String title;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String description;
   boolean isUrgent;
   boolean isDone;
+
+  public Todo() {
+  }
+
+  public Todo(String description) {
+    this.description = description;
+  }
+
+  public String toString() {
+    return id + " - " + description;
+  }
 }
