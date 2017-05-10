22056cbc0b2b200814408889268fac5dc3ba4863
zsiguli <markovichzsigmond@gmail.com>
2017-05-10 11:38:18 +0200

    fix(teplate): show values in a tabel

--- week-08/day-02/connect-with-mysql/src/main/java/com/greenfox/model/Todo.java
+++ week-08/day-02/connect-with-mysql/src/main/java/com/greenfox/model/Todo.java
@@ -15,8 +15,8 @@ public class Todo {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
   private String description;
-  boolean isUrgent;
-  boolean isDone;
+  private boolean isUrgent;
+  private boolean isDone;
 
   public Todo() {
   }
