13a7cb17ecbff29ae7d8384f2c73ec4f739996ec
zsiguli <markovichzsigmond@gmail.com>
2017-05-14 14:04:52 +0200

    chore(nameChange)

--- week-08/day-05/reddit-backand/src/main/java/com/greenfox/zsiguli/reddit/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.greenfox.zsiguli.reddit.repository;
-
-import com.greenfox.zsiguli.reddit.model.Post;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PostRepository extends CrudRepository<Post, Long> {
-}
