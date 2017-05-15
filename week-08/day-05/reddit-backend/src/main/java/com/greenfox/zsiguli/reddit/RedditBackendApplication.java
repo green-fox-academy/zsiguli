68b468b10a7e8d797bd82c0faf8ec055abb989ea
zsiguli <markovichzsigmond@gmail.com>
2017-05-15 07:28:25 +0200

    fix(app): rename

--- week-08/day-05/reddit-backend/src/main/java/com/greenfox/zsiguli/reddit/RedditBackendApplication.java
+++ week-08/day-05/reddit-backend/src/main/java/com/greenfox/zsiguli/reddit/RedditBackendApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RedditBackandApplication {
+public class RedditBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RedditBackandApplication.class, args);
+		SpringApplication.run(RedditBackendApplication.class, args);
 	}
 }
