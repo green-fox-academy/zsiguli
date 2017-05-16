45f6b23d570cbe8c9ea87d3d381d53bf7b1a17de
zsiguli <markovichzsigmond@gmail.com>
2017-05-16 11:57:03 +0200

    feat(post): can add new post

--- week-09/day-02/reddit/src/main/java/com/greenfox/reddit/controller/PostController.java
+++ week-09/day-02/reddit/src/main/java/com/greenfox/reddit/controller/PostController.java
@@ -1,9 +1,12 @@
 package com.greenfox.reddit.controller;
 
 import com.greenfox.reddit.repository.PostRepository;
+import com.greenfox.reddit.service.Post;
 import com.greenfox.reddit.service.PostContainer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +18,11 @@ public class PostController {
   @GetMapping("/posts")
   public PostContainer sendBackAllAvailablePosts() {
     return new PostContainer(postRepository.findAll());
+  }
+
+  @PostMapping("/posts")
+  public Post addPost(@RequestBody Post post) {
+    postRepository.save(post);
+    return post;
   }
 }
