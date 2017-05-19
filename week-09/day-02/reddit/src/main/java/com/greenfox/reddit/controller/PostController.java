3910dd5775b2e084a32f3b542b1dc9e840d8772b
zsiguli <markovichzsigmond@gmail.com>
2017-05-19 11:35:25 +0200

    reafctor(reddit): vote logic separated from controller to service

--- week-09/day-02/reddit/src/main/java/com/greenfox/reddit/controller/PostController.java
+++ week-09/day-02/reddit/src/main/java/com/greenfox/reddit/controller/PostController.java
@@ -12,29 +12,27 @@ public class PostController {
   @Autowired
   PostRepository postRepository;
 
+  @Autowired
+  PostContainer postContainer;
+
   @GetMapping("/posts")
   public PostContainer sendBackAllAvailablePosts() {
-    return new PostContainer(postRepository.findAllByOrderByScoreDesc());
+    return postContainer.findAllAvailablePosts();
   }
 
   @PostMapping("/posts")
   public Post addPost(@RequestBody Post post) {
-    postRepository.save(post);
-    return post;
+    return postContainer.addPost(post);
   }
 
   @PutMapping("/posts/{id}/upvote")
   public Post upVote(@PathVariable long id) {
-    PostContainer postContainer = new PostContainer(postRepository.findAllByOrderByScoreDesc());
-    postRepository.save(postContainer.findSelected(id).incScore());
-    return postContainer.findSelected(id);
+    return postContainer.upVote(id);
   }
 
   @PutMapping("/posts/{id}/downvote")
   public Post downVote(@PathVariable long id) {
-    PostContainer postContainer = new PostContainer(postRepository.findAllByOrderByScoreDesc());
-    postRepository.save(postContainer.findSelected(id).decScore());
-    return postContainer.findSelected(id);
+    return postContainer.downVote(id);
   }
 
   @DeleteMapping("/posts/{id}")
--- week-09/day-02/reddit/src/main/java/com/greenfox/reddit/service/PostContainer.java
+++ week-09/day-02/reddit/src/main/java/com/greenfox/reddit/service/PostContainer.java
@@ -1,14 +1,46 @@
 package com.greenfox.reddit.service;
 
+import com.greenfox.reddit.repository.PostRepository;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Getter
+@NoArgsConstructor
+@Service
 public class PostContainer {
+
+  @Autowired
+  PostRepository postRepository;
 
   private Iterable<Post> posts;
 
   public PostContainer(Iterable<Post> posts) {
     this.posts = posts;
+  }
+
+  public PostContainer findAllAvailablePosts() {
+    return new PostContainer(postRepository.findAllByOrderByScoreDesc());
+  }
+
+  public Post addPost(Post post) {
+    postRepository.save(post);
+    return post;
+  }
+
+  public Post upVote(long id) {
+    posts = postRepository.findAllByOrderByScoreDesc();
+    Post upVoted = findSelected(id).incScore();
+    postRepository.save(upVoted);
+    return upVoted;
+  }
+
+  public Post downVote(long id) {
+    posts = postRepository.findAllByOrderByScoreDesc();
+    Post downVoted = findSelected(id).decScore();
+    postRepository.save(downVoted);
+    return downVoted;
   }
 
   public Post findSelected(long id) {
